934dba2f6c1177f55c63ff81d1a281ffc51e8cac
khalid <karazani2003@gmail.com>
2026-05-08 19:39:30 +0100

    587

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,8 +1,3 @@
-
-
-
-
-
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -10,9 +5,7 @@ import "@fontsource/roboto/700.css";
 
 import {
   Box,
-
   Card,
-
   Container,
 } from "@mui/material";
 
