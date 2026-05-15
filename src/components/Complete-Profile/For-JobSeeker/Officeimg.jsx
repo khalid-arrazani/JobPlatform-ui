edc294795f6cf058c41ecb412fa528f3d5f07f58
khalid <karazani2003@gmail.com>
2026-05-15 22:50:12 +0100

    JobSeekerCPPage

---
+++ src/components/Complete-Profile/For-JobSeeker/Officeimg.jsx
@@ -0,0 +1,24 @@
+import logocompleteprofile from "../../../assets/Office work-pana.svg";
+export default function OfficePart() {
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+          src={logocompleteprofile}
+          width={"80%"}
+          alt=""
+        />
+      </div>
+    </>
+  );
+}
