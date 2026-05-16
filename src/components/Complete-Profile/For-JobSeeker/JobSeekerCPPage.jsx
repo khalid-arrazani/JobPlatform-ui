62c65e8eaf307596ebec427e66f0238c1b730dac
khalid <karazani2003@gmail.com>
2026-05-16 13:43:35 +0100

    12

--- src/components/Complete-Profile/For-JobSeeker/JobSeekerCPPage.jsx
+++ src/components/Complete-Profile/For-JobSeeker/JobSeekerCPPage.jsx
@@ -1,8 +1,10 @@
 import "./style.css";
-import { Card, Typography, Box } from "@mui/material";
+import { Card,  Box } from "@mui/material";
 
 import Header from "./header";
 import OfficePart from "./Officeimg";
+import WhyCompleteProfileCard from "./WhyComplete";
+import CompleteProfileDs from "./CompleteProfileDs";
 
 export default function JobSeekerCPPage() {
   return (
@@ -16,54 +18,17 @@ export default function JobSeekerCPPage() {
 
           <div className="container1">
             <div className="leftSide">
+
               <OfficePart />
+    
+              <CompleteProfileDs />
 
-              <div className="discr">
-                <Typography
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    width: "100%",
-                    fontWeight: "600",
-                    color: "#0e0d0dcb",
-                    fontSize: "1.7rem",
-                  }}
-                >
-                  Complete Your Profile
-                </Typography>
-                <Typography
-                  variant="h7"
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    width: "90%",
-                    fontWeight: "500",
-                    color: "#0e0d0dcb",
-                    pt: "0.8rem",
-                  }}
-                >
-                  Let's create your professional identity.Complete your profile
-                  to unlock the best job opportunities and get noticed by
-                  recruiters.
-                </Typography>
-                <Box sx={{ width: "95%" }}>
-                  <Typography
-                    sx={{
-                      display: "flex",
 
-                      width: "100%",
-                      fontWeight: "700",
-                      color: "#0e0d0dc9",
-                      fontSize: "0.8rem",
-                      pt: "1rem",
-                    }}
-                  >
-                    Why complete your profile?
-                  </Typography>
-                </Box>
-              </div>
+              
+                <WhyCompleteProfileCard />
+                
+              
 
-              <div></div>
             </div>
 
             <div className="rightSide"></div>
