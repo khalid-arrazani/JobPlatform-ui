ca4beddb2371bfd9f2d67333f23f7b83d2b13551
khalid <karazani2003@gmail.com>
2026-05-17 21:12:40 +0100

    UploadProfilePhoto

--- src/components/Complete-Profile/For-Recruiter/BasicInformationSection.jsx
+++ src/components/Complete-Profile/For-Recruiter/BasicInformationSection.jsx
@@ -2,15 +2,13 @@ import {
   Box,
   Typography,
   Chip,
-  Avatar,
+
   Button,
   TextField,
 } from "@mui/material";
-import { useState,useRef } from "react";
 
-import AvatarEditor from "react-avatar-editor";
 
-import { Dialog, DialogContent, Slider } from "@mui/material";
+import UploadProfilePhoto from "./UploadProfilePhoto"
 
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
@@ -19,28 +17,11 @@ import TrendingFlatOutlinedIcon from "@mui/icons-material/TrendingFlatOutlined";
 
 import Divider from "@mui/material/Divider";
 
+
 export default function BasicInformationSection() {
   
-  const editorRef = useRef();
-const [image, setImage] = useState(null);
 
-  const [scale, setScale] = useState(1.2);
-
-  const [open, setOpen] = useState(false);
-
-  const handleSave = () => {
-    const canvas =
-      editorRef.current.getImageScaledToCanvas();
-
-    const croppedImage =
-      canvas.toDataURL();
-
-    console.log(croppedImage);
-
-    setImage(croppedImage);
-
-    setOpen(false);
-  };
+  
   return (
     <>
       {/* Badge */}
@@ -81,137 +62,9 @@ const [image, setImage] = useState(null);
         Start by telling us a bit about yourself and your Company
       </Typography>
 
-      {/* Upload */}
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          gap: "1rem",
-          mb: "1rem",
-          flexWrap: "wrap",
-        }}
-      >
-        {/* Avatar */}
-        <Box
-          sx={{
-            position: "relative",
-            width: "5.5rem",
-            height: "5.5rem",
-          }}
-        >
-          <Avatar
-            src={image}
-            sx={{
-              width: "100%",
-              height: "100%",
-              background: "#ede9fe",
-              borderRadius: "10px",
-            }}
-          />
-        </Box>
+      <UploadProfilePhoto/>
 
-        {/* Upload Text */}
-        <Box>
-          <Typography
-            sx={{
-              fontSize: "0.9rem",
-              fontWeight: 600,
-              color: "#111827",
-              mb: "0.3rem",
-            }}
-          >
-            Upload profile photo
-          </Typography>
 
-          <Typography
-            sx={{
-              fontSize: "0.75rem",
-              color: "#6b7280",
-              mb: "0.5rem",
-            }}
-          >
-            JPG or PNG • Max 2MB
-          </Typography>
-
-          <Button
-            component="label"
-            size="small"
-            variant="outlined"
-            sx={{
-              borderRadius: "0.4rem",
-              textTransform: "none",
-              borderColor: "#d6d3f0",
-              color: "#5b21b6",
-              fontWeight: 600,
-              fontSize: "0.72rem",
-
-              "&:hover": {
-                borderColor: "#5b21b6",
-                background: "#f8f5ff",
-              },
-            }}
-          >
-            Choose File
-            <input
-              hidden
-              accept="image/*"
-              type="file"
-              onChange={(e) => {
-                const file = e.target.files[0];
-
-                if (file) {
-             
-                  setImage(file);
-                  setOpen(true);
-                }
-              }}
-            />
-          </Button>
-        </Box>
-      </Box>
-
-     <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-      >
-        <DialogContent>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              gap: "1rem",
-            }}
-          >
-            <AvatarEditor
-              ref={editorRef}
-              image={image}
-              width={350}
-              height={350}
-              border={20}
-              borderRadius={200}
-              scale={scale}
-            />
-
-            <Slider
-              min={1}
-              max={3}
-              step={0.1}
-              value={scale}
-              onChange={(e, value) =>
-                setScale(value)
-              }
-            />
-
-            <Button
-              variant="contained"
-              onClick={handleSave}
-            >
-              Save
-            </Button>
-          </Box>
-        </DialogContent>
-      </Dialog>
 
       {/* Full Name */}
       <Box sx={{ mb: "1rem" }}>
---
+++ src/components/Complete-Profile/For-Recruiter/UploadProfilePhoto.jsx
@@ -0,0 +1,175 @@
+import {
+  Box,
+  Typography,
+
+  Avatar,
+  Button,
+
+} from "@mui/material";
+
+import { useState, useRef } from "react";
+
+import AvatarEditor from "react-avatar-editor";
+
+import { Dialog, DialogContent, Slider } from "@mui/material";
+
+export default function UploadProfilePhoto() {
+  
+  const editorRef = useRef();
+
+  const [image, setImage] = useState(null);
+
+  const [scale, setScale] = useState(1.2);
+
+  const [open, setOpen] = useState(false);
+
+
+  const getBlob = (canvas) => {
+  return new Promise((resolve) => {
+    canvas.toBlob((blob) => {
+      resolve(blob);
+    }, "image/png");
+  });
+};
+
+
+const handleSave = async () => {
+
+  const canvas = editorRef.current.getImageScaledToCanvas();
+  const blob = await getBlob(canvas);
+  console.log(blob);
+  const url = URL.createObjectURL(blob);
+  setImage(url);
+  console.log(url);
+  setOpen(false);
+  
+};
+
+  return (
+    
+    <>
+      {/* Upload */}
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "1rem",
+          mb: "1rem",
+          flexWrap: "wrap",
+        }}
+      >
+        {/* Avatar */}
+        <Box
+          sx={{
+            position: "relative",
+            width: "5.5rem",
+            height: "5.5rem",
+          }}
+        >
+          <Avatar
+            src={image}
+            sx={{
+              width: "100%",
+              height: "100%",
+              background: "#ede9fe",
+              borderRadius: "10px",
+            }}
+          />
+        </Box>
+
+        {/* Upload Text */}
+        <Box>
+          <Typography
+            sx={{
+              fontSize: "0.9rem",
+              fontWeight: 600,
+              color: "#111827",
+              mb: "0.3rem",
+            }}
+          >
+            Upload profile photo
+          </Typography>
+
+          <Typography
+            sx={{
+              fontSize: "0.75rem",
+              color: "#6b7280",
+              mb: "0.5rem",
+            }}
+          >
+            JPG or PNG • Max 2MB
+          </Typography>
+
+          <Button
+            component="label"
+            size="small"
+            variant="outlined"
+            sx={{
+              borderRadius: "0.4rem",
+              textTransform: "none",
+              borderColor: "#d6d3f0",
+              color: "#5b21b6",
+              fontWeight: 600,
+              fontSize: "0.72rem",
+
+              "&:hover": {
+                borderColor: "#5b21b6",
+                background: "#f8f5ff",
+              },
+            }}
+          >
+            Choose File
+            <input
+              hidden
+              accept="image/*"
+              type="file"
+              onChange={(e) => {
+                const file = e.target.files[0];
+                console.log(file);
+
+                if (file) {
+                  setImage(file);
+                  setOpen(true);
+                }
+              }}
+            />
+          </Button>
+        </Box>
+      </Box>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogContent>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "1rem",
+            }}
+          >
+            <AvatarEditor
+              ref={editorRef}
+              image={image}
+              width={350}
+              height={350}
+              border={20}
+              borderRadius={200}
+              scale={scale}
+            />
+
+            <Slider
+              min={1}
+              max={3}
+              step={0.1}
+              value={scale}
+              onChange={(e, value) => setScale(value)}
+            />
+
+            <Button variant="contained" onClick={handleSave}>
+              Save
+            </Button>
+          </Box>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
