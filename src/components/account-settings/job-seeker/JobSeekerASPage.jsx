af9377a8d3d0ca5c1e490b4e6d98ed5dfb20c94b
khalid <karazani2003@gmail.com>
2026-05-23 22:34:40 +0100

    11

--- src/components/account-settings/job-seeker/JobSeekerASPage.jsx
+++ src/components/account-settings/job-seeker/JobSeekerASPage.jsx
@@ -24,7 +24,7 @@ export default function JobSeekerASPage() {
             </div>
 
             <div className="JobSeekerrightSide">
-              <SecuritySettings />
+              <SocialLinksSettings />
             </div>
           </div>
         </Card>
