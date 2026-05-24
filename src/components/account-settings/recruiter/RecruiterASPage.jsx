c9b36b3a361dda90e7fc338a89c2b455bb985abe
khalid <karazani2003@gmail.com>
2026-05-24 13:09:02 +0100

    11

---
+++ src/components/account-settings/recruiter/RecruiterASPage.jsx
@@ -0,0 +1,62 @@
+import "./RecruiterASStyle.css";
+import { Card } from "@mui/material";
+
+// import SettingesHeader from "./SettingesHeader";
+// import JobSeekerSettingsSidebar from "./JobSeekerSettingsSidebar";
+
+// import ProfileInformation from "./SettingsPages/ProfileInformation";
+// import SecuritySettings from "./SettingsPages/SecuritySettings";
+// import SocialLinksSettings from "./SettingsPages/SocialLinksSettings";
+// import CVSettings from "./SettingsPages/CVSettings";
+
+import { useState } from "react";
+
+export default function RecruiterASPage() {
+
+  // const [tap, setTap] = useState()
+ 
+
+// const renderTab = () => {
+//   switch (tap) {
+//     case "profile":
+//       return <ProfileInformation />;
+
+//     case "security":
+//       return <SecuritySettings />;
+
+//     case "social":
+//       return <SocialLinksSettings />;
+
+//     case "cv":
+//       return <CVSettings />;
+
+//     default:
+//       return <ProfileInformation />;
+//   }
+// };
+
+  return (
+    <>
+      <div className="RecruiterASpar">
+        <Card
+          className="RecruiterAScontainer"
+          sx={{ borderRadius: "0.6rem", boxShadow: "none" }}
+        >
+
+
+          <div className="container1">
+
+            <div className="leftSide">
+              
+            </div>
+
+            <div className="RecruiterrightSide">
+              
+         
+            </div>
+          </div>
+        </Card>
+      </div>
+    </>
+  );
+}
--- src/pages/AccountSettingsPage.jsx
+++ src/pages/AccountSettingsPage.jsx
@@ -1,7 +1,7 @@
 
 import AccountSettingsLayout from "../layouts/AccountSettingsLayout"
 import JobSeekerASPage  from "../components/account-settings/job-seeker/JobSeekerASPage"
-
+import RecruiterASPage from ".."
 export default function AccountSettingsPage() {
   return (
     <AccountSettingsLayout>
