6c54b8389657b16ced2cddae06509d8f9077d27e
khalid <karazani2003@gmail.com>
2026-05-26 16:13:23 +0100

    11

--- src/SnackBars/SnackBars.jsx
+++ src/SnackBars/SnackBars.jsx
@@ -1,41 +1,36 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import Alert from '@mui/material/Alert';
-import CheckIcon from '@mui/icons-material/Check';
+import * as React from "react";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
 export default function SimpleSnackbar() {
-
   const [open, setOpen] = React.useState(true);
 
-
-
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
     setOpen(false);
   };
 
-  const action = (
-    <>
-    <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
-      Here is a gentle confirmation that your action was successful.
-    </Alert>
-    </>
-  );
-
   return (
-    <div style={{}}>
-
-      <Snackbar
-        open={open}
-        autoHideDuration={6000}
+    <Snackbar
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+    >
+      <Alert
         onClose={handleClose}
-        action={action}
-        sx={{m:0,p:0}}
-      />
-    </div>
+        severity="success"
+        variant="filled"
+        sx={{ width: "100%" }}
+      >
+        Registered successfully
+      </Alert>
+    </Snackbar>
   );
 }
