a6a486ecdeefd5a7ec2e8c8758645f8ab7bc9ebc
khalid <karazani2003@gmail.com>
2026-05-28 14:49:55 +0100

    44

--- src/logic/api/config/axios.jsx
+++ src/logic/api/config/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "http://localhost:5000/api", withCredentials: true,
 });
 
 export default API;
