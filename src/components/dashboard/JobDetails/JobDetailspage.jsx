811bf2cb64cca5108edabeb6fdb8faf27dc96e4a
khalid <karazani2003@gmail.com>
2026-06-11 23:19:23 +0100

    11

--- src/components/dashboard/JobDetails/JobDetailspage.jsx
+++ src/components/dashboard/JobDetails/JobDetailspage.jsx
@@ -56,6 +56,7 @@ export default function JobDetailsPage() {
             width: "75%",
             background: "#f7f7f7",
             pl: "1.9rem",
+            
           }}
         >
           <Button
@@ -64,7 +65,7 @@ export default function JobDetailsPage() {
             sx={{
               textTransform: "none",
               fontWeight: 600,
-              color: "#f940fcec",
+              color: "#ae2eb0ec",
               borderRadius: "12px",
               px: 1.5,
 
@@ -85,92 +86,128 @@ export default function JobDetailsPage() {
 
           <Card
             sx={{
-              flex:1,
-              height:"10rem",
-              px: 4,
-              py: 2,
-              borderRadius: "28px",
+              flex: 1,
+              height: "auto",
+
+              borderRadius: "10px",
               boxShadow: "0 8px 30px rgba(0,0,0,0.06)",
               display: "flex",
               justifyContent: "space-between",
               alignItems: "center",
               bgcolor: "#fff",
-              mb: 3,
+              pr: "1rem",
             }}
           >
-
-          <Box  sx={{display:"flex",}}>
-            <Avatar
-              src="https://logo.clearbit.com/google.com"
+            <Box
               sx={{
-                width: 110,
-                height: 110,
-                bgcolor: "#fff",
-                border: "1px solid #eee",
-                borderRadius:"10px",
-                m:"1rem"
+                display: "flex",
+                alignItems: "center",
               }}
-            />
+            >
+              <Avatar
+                src="https://i.pinimgproxy.com/?url=aHR0cHM6Ly9jZG4taWNvbnMtcG5nLmZsYXRpY29uLmNvbS8yNTYvMjgxLzI4MTc2NC5wbmc=&ts=1781217189&sig=ab15924525ba4834dae1622a3a8c25e61e488fad2189d2f0bb622776e27e9a24"
+                sx={{
+                  width: 90,
+                  height: 90,
+                  bgcolor: "#fff",
+                  border: "1px solid #eee",
+                  borderRadius: "10px",
+                  m: "2rem",
+                  p:2
+                }}
+              />
 
-            {/* Left Side */}
-            <Box display="flex" alignItems="center" gap={3}>
-              <Box>
-                <Typography
-                  sx={{
-                    fontSize: "2rem",
-                    fontWeight: 700,
-                    color: "#1E1E1E",
-                  }}
-                >
-                  Senior UI/UX Designer
-                </Typography>
-
-                <Typography
-                  sx={{
-                    color: "#666",
-                    mt: 0.5,
-                    fontSize: "0.95rem",
-                  }}
-                >
-                  Google • San Francisco, CA
-                </Typography>
-
-                <Stack direction="row" spacing={1} mt={2}>
-                  <Chip
-                    label="Full Time"
+              {/* Left Side */}
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  borderLeft: "solid 1px #d4d3d3",
+                  pl: "1.2rem",
+                  height: "fit-Content",
+                }}
+              >
+                <Box>
+                    <Stack direction="row" spacing={1.5}>
+                    <Chip
+                      label="Verified Company"
+                      size="small"
+                      sx={{
+                        bgcolor: "#e8ffea",
+                        color: "#238a3f",
+                        fontWeight: 400,
+                      }}
+                    />
+                  </Stack>
+                  <Typography
                     sx={{
-                      bgcolor: "#F3E8FF",
-                      color: "#7C3AED",
-                      fontWeight: 600,
+                      fontSize: "1.7rem",
+                      fontWeight: 700,
+                      color: "#060505e8",
                     }}
-                  />
-
-                  <Chip
-                    label="Senior Level"
-                    sx={{
-                      bgcolor: "#EEF2FF",
-                      color: "#4F46E5",
-                      fontWeight: 600,
-                    }}
-                  />
-                </Stack>
-
-                <Stack direction="row" spacing={3} mt={2}>
-                  <Typography color="text.secondary">
-                    👥 128 Applicants
+                  >
+                    Senior UI/UX Designer
                   </Typography>
 
-                  <Typography color="text.secondary">🔖 842 Saved</Typography>
-
-                  <Typography color="text.secondary">
-                    💰 $95k – $120k
+                  <Typography
+                    sx={{
+                      color: "#000000",
+                      mt: 0.5,
+                      fontSize: "0.95rem",
+                      mb: 1.5,
+                    }}
+                  >
+                    Google • San Francisco, CA
                   </Typography>
-                </Stack>
+
+                  <Stack direction="row" spacing={1.5}>
+                    <Chip
+                      label="Full Time"
+                      size="small"
+                      sx={{
+                        bgcolor: "#fce8ff",
+                        color: "#7C3AED",
+                        fontWeight: 400,
+                      }}
+                    />
+
+                    <Chip
+                      label="Senior Level"
+                      size="small"
+                      sx={{
+                        bgcolor: "#fce8ff",
+                        color: "#7C3AED",
+                        fontWeight: 400,
+                      }}
+                    />
+                    <Chip
+                      label="$95k – $120k / year"
+                      size="small"
+                      sx={{
+                        bgcolor: "#e9fcf1",
+                        color: "#27a25e",
+                        fontWeight: 400,
+                      }}
+                    />
+
+                    <Chip
+                      label="Posted 2 days ago"
+                      size="small"
+                      sx={{
+                        bgcolor: "#f2f0f0",
+                        color: "#404040c5",
+                        fontWeight: 400,
+                      }}
+                    />
+                   
+                  </Stack>
+                </Box>
               </Box>
             </Box>
-</Box>
             {/* Right Side */}
-            <Stack spacing={2}>
+            <Stack direction="row" spacing={2}>
+
               <Button
                 variant="outlined"
                 startIcon={<TurnedInNotOutlinedIcon />}
