5c1f8887f8b0a9bcf53503e99fa18a8e0702a556
khalid <karazani2003@gmail.com>
2026-06-13 11:38:12 +0100

    11

--- src/components/dashboard/JobDetails/CardJobOverview.jsx
+++ src/components/dashboard/JobDetails/CardJobOverview.jsx
@@ -1,18 +1,12 @@
 import {
-  Box,
-  Card,
-  Typography,
-  Avatar,
-  Chip,
-  Button,
-  Stack,
-  Divider,
+    Card
+
 } from "@mui/material";
 
 export default function JobOverview(){
 
     return <>
-    <Card sx={{width:"90%",height:"50%",mt:1, boxShadow: "0 8px 30px rgba(0,0,0,0.06)"}}>
+    <Card sx={{width:"18.5rem",height:"50%",mt:1, boxShadow: "0 8px 30px rgba(0,0,0,0.06)"}}>
 
 
     </Card>
