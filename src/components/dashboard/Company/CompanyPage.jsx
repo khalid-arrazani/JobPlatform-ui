5ad86cf41a5b17739966bf5b2bbbbd6020d23a94
khalid <karazani2003@gmail.com>
2026-06-17 21:52:14 +0100

    55

--- src/components/dashboard/Company/CompanyPage.jsx
+++ src/components/dashboard/Company/CompanyPage.jsx
@@ -1,14 +1,14 @@
 
 import { Box } from "@mui/material"
 
+import CompanyHeader from "./CompanyHeader.jsx"
+
 export default function CompanyPage (){
 
     return <>
     <Box sx={{width:"100vw" , height:"92.4vh" , bgcolor:"#f3f5f9" ,boxSizing:"border-box",paddingX:5,pt:3}}>
-        <Box  sx={{width:"100%" , height:"45vh" , bgcolor:"#e4e4e4",borderRadius:"12px" }}>
-            
-        </Box>
-
+   
+          <CompanyHeader/>
     </Box>
     
     </>
