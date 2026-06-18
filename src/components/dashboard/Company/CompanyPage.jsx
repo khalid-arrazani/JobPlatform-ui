ca8df5211f32db76b283accef7996d0ab213c0d8
khalid <karazani2003@gmail.com>
2026-06-18 10:56:14 +0100

    11

--- src/components/dashboard/Company/CompanyPage.jsx
+++ src/components/dashboard/Company/CompanyPage.jsx
@@ -2,7 +2,9 @@ import { Box } from "@mui/material";
 export default function CompanyPage() {
   return (
     <>
-      <Box sx={{ height: "100% ", width: "100vw", bgcolor: "#c7cddb" }}></Box>
+      <Box sx={{ height: "92.3vh ", width: "100vw", bgcolor: "#000000" }}>
+
+      </Box>
     </>
   );
 }
