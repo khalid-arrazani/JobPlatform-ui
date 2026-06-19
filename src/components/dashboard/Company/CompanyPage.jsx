981dd97d9d19e01f8a85fffa85945fa26fa79ec2
khalid <karazani2003@gmail.com>
2026-06-19 11:39:38 +0100

    11

--- src/components/dashboard/Company/CompanyPage.jsx
+++ src/components/dashboard/Company/CompanyPage.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box,Card } from "@mui/material";
 import CompanyHeader from "./CompanyHeader";
 
 export default function CompanyPage() {
@@ -10,15 +10,26 @@ export default function CompanyPage() {
           width: "100vw",
           bgcolor: "#bebfcd",
           boxSizing: "border-box",
-          px: 10,
+          px: 6,
           pt: 3,
+          overflow:"auto"
         }}
       >
 
      <CompanyHeader/>
 
-     <Box>
-        
+     <Box sx={{width:"100%" , height:"100vh" , bgcolor:"#ffffff00",my:1.5,borderRadius:"15px",display:"flex",gap:1.5}}>
+        <Box sx={{width:"50%" , height:"100vh" , bgcolor:"#47474768",borderRadius:"15px"}}>
+
+            <Card sx={{height:"10rem",width:"100%",borderRadius:"15px"}}>
+
+
+            </Card>
+
+        </Box>
+
+        <Box sx={{width:"50%" , height:"100vh" , bgcolor:"#8c878768",borderRadius:"15px"}}></Box>
+
      </Box>
       
 
