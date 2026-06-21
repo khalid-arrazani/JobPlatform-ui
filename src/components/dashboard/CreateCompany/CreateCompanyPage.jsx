d5bd69b085e66112da3e3dbd67042f6ea24425bf
khalid <karazani2003@gmail.com>
2026-06-21 20:00:40 +0100

    11

--- src/components/dashboard/CreateCompany/CreateCompanyPage.jsx
+++ src/components/dashboard/CreateCompany/CreateCompanyPage.jsx
@@ -33,6 +33,8 @@ export default function CreateCompanyPage() {
               borderRadius: "10px",
               position: "relative",
               overflow: "hidden",
+              boxSizing: "border-box",
+              px: 1,
             }}
           >
             <Box
@@ -43,24 +45,62 @@ export default function CreateCompanyPage() {
                 justifyContent: "center",
               }}
             >
-             
               <img src={createCompany} alt="Idea" />
-             
-              
             </Box>
 
-             <Typography sx={{textAlign:"center",fontFamily:'ui-monospace',fontWeight:600,color:"#1a1a25e4"}}> Create your company profile</Typography>
+            <Typography
+              sx={{
+                textAlign: "center",
+                fontFamily: "ui-monospace",
+                fontWeight: 600,
+                color: "#1a1a25e4",
+                mb: 1,
+              }}
+            >
+              {" "}
+              Create your company profile
+            </Typography>
 
+            <Typography
+              sx={{
+                textAlign: "center",
+                fontFamily: "ui-rounded",
+                fontWeight: 600,
+                color: "#010101a8",
+                textWrap: "wrap",
+                fontSize: "0.8rem",
+              }}
+            >
+              {" "}
+              A complete profile helps candidates learn more about your company.
+            </Typography>
           </Card>
 
-          <Box
+          <Card
             sx={{
               height: "50%",
-              bgcolor: "#424141",
+              bgcolor: "#ffffff",
               width: "100%",
               borderRadius: "10px",
+              overflow: "hidden",
+              boxSizing: "border-box",
+              px: 2.5,py:2
             }}
-          ></Box>
+          >
+           <Typography
+              sx={{
+                
+                fontFamily: "ui-monospace",
+                fontWeight: 600,
+                color: "#1a1a25de",
+                mb: 1,
+                
+              }}
+            >
+              {" "}
+              Why is this important?
+            </Typography>
+          </Card>
         </Box>
 
         <Box sx={{ height: "100%", bgcolor: "#fff", width: "75%" }}></Box>
