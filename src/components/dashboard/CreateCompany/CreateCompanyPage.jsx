6fdcb3480934f8728433ae1c6a6648b748eeb595
khalid <karazani2003@gmail.com>
2026-06-21 15:35:44 +0100

    11

--- src/components/dashboard/CreateCompany/CreateCompanyPage.jsx
+++ src/components/dashboard/CreateCompany/CreateCompanyPage.jsx
@@ -1,11 +1,26 @@
-import { Card, Typography, Box,  Divider } from "@mui/material";
+import { Card, Typography, Box, Divider } from "@mui/material";
 
+export default function CreateCompanyPage() {
+  return (
+    <>
+      <Box
+        sx={{
+          height: "100%",
+          width: "100vw",
+          bgcolor: "#dfdfdf",
+          display: "flex",
+          boxSizing: "border-box",
+          gap:1
+        }}
+      >
+        <Box sx={{height:"100%",bgcolor:"#fff",width:"30%"}}>
+            
+        </Box>
 
+        <Box sx={{height:"100%",bgcolor:"#fff",width:"70%"}}>
 
-export default function CreateCompanyPage(){
-
-    return <>
-    <Box sx={{height:"100%",width:"100vw",bgcolor:"#272525"}}></Box>
-    
+        </Box>
+      </Box>
     </>
+  );
 }
