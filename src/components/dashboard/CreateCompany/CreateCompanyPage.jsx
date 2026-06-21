2b81ca6fe146ce65184c5953c97d6623ae4c717d
khalid <karazani2003@gmail.com>
2026-06-21 17:53:41 +0100

    111

--- src/components/dashboard/CreateCompany/CreateCompanyPage.jsx
+++ src/components/dashboard/CreateCompany/CreateCompanyPage.jsx
@@ -1,4 +1,5 @@
 import { Card, Typography, Box, Divider } from "@mui/material";
+import createCompany from "./../../../assets/CreateCompany.svg";
 
 export default function CreateCompanyPage() {
   return (
@@ -10,16 +11,59 @@ export default function CreateCompanyPage() {
           bgcolor: "#dfdfdf",
           display: "flex",
           boxSizing: "border-box",
-          gap:1
+          gap: 1,
         }}
       >
-        <Box sx={{height:"100%",bgcolor:"#fff",width:"30%"}}>
-            
+        <Box
+          sx={{
+            height: "100%",
+            bgcolor: "#e8e6e6",
+            boxSizing: "border-box",
+            width: "25%",
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Card
+            sx={{
+              height: "35%",
+              width: "100%",
+              borderRadius: "10px",
+              position: "relative",
+              overflow: "hidden",
+            }}
+          >
+            <Box
+              sx={{
+                width: "100%",
+                height: "50%",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+             
+              <img src={createCompany} alt="Idea" />
+             
+              
+            </Box>
+
+             <Typography sx={{textAlign:"center",fontFamily:'ui-monospace',fontWeight:600,color:"#1a1a25e4"}}> Create your company profile</Typography>
+
+          </Card>
+
+          <Box
+            sx={{
+              height: "50%",
+              bgcolor: "#424141",
+              width: "100%",
+              borderRadius: "10px",
+            }}
+          ></Box>
         </Box>
 
-        <Box sx={{height:"100%",bgcolor:"#fff",width:"70%"}}>
-
-        </Box>
+        <Box sx={{ height: "100%", bgcolor: "#fff", width: "75%" }}></Box>
       </Box>
     </>
   );
