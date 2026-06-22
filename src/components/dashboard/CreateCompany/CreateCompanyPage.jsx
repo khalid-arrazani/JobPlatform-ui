4cc83c87f7a57858933c75f83211698f301e0472
khalid <karazani2003@gmail.com>
2026-06-22 12:11:57 +0100

    111

--- src/components/dashboard/CreateCompany/CreateCompanyPage.jsx
+++ src/components/dashboard/CreateCompany/CreateCompanyPage.jsx
@@ -1,4 +1,4 @@
-import { Card, Typography, Box, Divider,Button } from "@mui/material";
+import { Card, Typography, Box, Divider, Button } from "@mui/material";
 import Left_side from "./left_Side";
 
 import Stepper from "@mui/material/Stepper";
@@ -7,7 +7,9 @@ import StepLabel from "@mui/material/StepLabel";
 import { useState } from "react";
 
 export default function CreateCompanyPage() {
-    const [step , setStep] = useState(0)
+  const [step, setStep] = useState(0);
+
+  const steps = ["Company Information", "Additional Details", "Branding"];
 
   return (
     <>
@@ -31,36 +33,57 @@ export default function CreateCompanyPage() {
             width: "75%",
             borderLeft: "solid 0.5px #d5d5d5",
             py: 2,
-            position:"relative",
-            boxSizing:"border-box"
+            position: "relative",
+            boxSizing: "border-box",
           }}
         >
+            <Box>
 
+            <Typography> step {step + 1} of 3</Typography>
 
-          <Stepper  sx={{ width: "50%" }} activeStep={step}>
-            <Step sx={{ fontSize: "0.8rem" }}>
+          <Stepper
+            activeStep={step}
+            sx={{
+              width: "15%",
 
-              <StepLabel >
-                <Typography sx={{ fontSize: "0.8rem" }}>First step </Typography>
-              </StepLabel>
+              "& .MuiStepLabel-label": {
+                fontSize: "0.75rem",
+              },
 
-            </Step>
-
-            <Step>
-              <StepLabel><Typography sx={{ fontSize: "0.8rem" }}>second step</Typography></StepLabel>
-            </Step>
-
-            <Step>
-              <StepLabel><Typography sx={{ fontSize: "0.8rem" }}>Third step </Typography></StepLabel>
-            </Step>
+              "& .MuiStepIcon-root": {
+                fontSize: "1.1rem",
+              },
+            }}
+          >
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel></StepLabel>
+              </Step>
+            ))}
           </Stepper>
+          
+          </Box>
 
-          <Button onClick={()=>{setStep((prev)=> prev + 1)}} sx={{position:"absolute",bottom:1,right:10}}> Next Step</Button>
+          <Button
+            onClick={() => {
+              setStep((prev) => prev + 1);
+            }}
+            sx={{ position: "absolute", bottom: 1, right: 10 }}
+          >
+            {" "}
+            Next Step
+          </Button>
 
-          <Button disabled={step==0} onClick={()=>{setStep((prev)=> prev - 1)}} sx={{position:"absolute",bottom:1,left:10}}> Back Step</Button>
-
-
-
+          <Button
+            disabled={step == 0}
+            onClick={() => {
+              setStep((prev) => prev - 1);
+            }}
+            sx={{ position: "absolute", bottom: 1, left: 10 }}
+          >
+            {" "}
+            Back Step
+          </Button>
         </Box>
       </Box>
     </>
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -21,7 +21,7 @@ export default function DashboardPage() {
         <Route path="Jobs/Details/:JobId" element={<JobDetailsPage />} />
         <Route path="Companies" element={<CompaniesPage />} />
         <Route path="Saved" element={<SavedJobs />} />
-        <Route path="My_Company" element={<CompanyPage />} />
+        <Route path="My_Company" element={<CreateCompanyPage />} />
 
       </Routes>
 
