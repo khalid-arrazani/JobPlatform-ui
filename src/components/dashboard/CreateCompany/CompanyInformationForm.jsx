7c369b5156f4359b2c092a5999224606eba3ce86
khalid <karazani2003@gmail.com>
2026-06-24 13:21:58 +0100

    111

--- src/components/dashboard/CreateCompany/CompanyInformationForm.jsx
+++ src/components/dashboard/CreateCompany/CompanyInformationForm.jsx
@@ -142,6 +142,8 @@ export default function CompanyInformationForm() {
 
           <TextField
             placeholder="Select industry"
+            
+
             fullWidth
             size="small"
             defaultValue=""
