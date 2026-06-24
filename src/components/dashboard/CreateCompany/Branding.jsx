6d2dfdfc1547bf7a518b163cff0ad65d18f4071a
khalid <karazani2003@gmail.com>
2026-06-24 21:03:06 +0100

    1111

--- src/components/dashboard/CreateCompany/Branding.jsx
+++ src/components/dashboard/CreateCompany/Branding.jsx
@@ -1,8 +1,11 @@
-import { Box, Card,IconButton } from "@mui/material";
+import { Box, Card, IconButton, Typography, Button } from "@mui/material";
 import DomainIcon from "@mui/icons-material/Domain";
 
-import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import PanoramaOutlinedIcon from "@mui/icons-material/PanoramaOutlined";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+
+import { MdOutlineFileUpload } from "react-icons/md";
 
 export default function Branding() {
   return (
@@ -25,13 +28,66 @@ export default function Branding() {
             borderRadius: "15px",
             overflow: "hidden",
             border: "solid 0.5px #b3b0b069",
-            position:"relative"
+            position: "relative",
           }}
         >
-          <Box sx={{ width: "100%", height: "68%", bgcolor: "#ffffff" }}>
-            <IconButton sx={{position:"absolute",bgcolor:"#123ff4",color:"#fff"}}>
-                <AddPhotoAlternateIcon  sx={{width:"3rem",height:"3rem"}}/>
-              </IconButton>
+          <Box
+            sx={{
+              width: "100%",
+              height: "68%",
+              bgcolor: "#ebebeb",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 0.5,
+            }}
+          >
+            <PanoramaOutlinedIcon
+              sx={{ width: "3rem", height: "3rem", color: "#0958c6" }}
+            />
+
+            <Typography
+              sx={{
+                fontFamily: "-apple-system",
+                fontWeight: 700,
+                color: "#0b0b12d9",
+                m: 0,
+              }}
+            >
+              Upload cover background
+            </Typography>
+
+            <Typography
+              sx={{
+                fontFamily: "-apple-system",
+                fontWeight: 700,
+                color: "#0a0a189f",
+                m: 0,
+                fontSize: "0.8rem",
+                p: 0,
+              }}
+            >
+              JBG or PNG. Recommended size 1920x600px. Max size 5MB
+            </Typography>
+
+            <Button
+              sx={{
+                color: "#4a62ff",
+                bgcolor: "#ffffff",
+                p: 0.8,
+                display: "flex",
+                gap: 0.5,
+                alignItems: "center",
+                borderRadius: "5px",
+                mt: 1.5,textTransform:"none",
+              }}
+            >
+              <MdOutlineFileUpload
+                style={{ width: "1.3rem", height: "1.3rem", p: 0 }}
+              />
+              Choose Image
+            </Button>
           </Box>
 
           <Box
@@ -60,13 +116,21 @@ export default function Branding() {
               }}
             >
               <DomainIcon
-                sx={{ width: "70%", height: "70%", color: "#d3d3d3" }}
+                sx={{ width: "70%", height: "70%", color: "#b1afaf" }}
               />
 
-              <IconButton sx={{position:"absolute",bottom:-10,right:-25,bgcolor:"#123ff4",color:"#fff"}}>
-                <AddAPhotoIcon  sx={{width:"3rem",height:"3rem"}}/>
+              <IconButton
+                sx={{
+                  position: "absolute",
+                  bottom: -9,
+                  right: -20,
+                  bgcolor: "#0033ffd0",
+                  color: "#fff",
+                  p: 1,
+                }}
+              >
+                <PhotoCameraIcon sx={{ width: "1.6rem", height: "1.6rem" }} />
               </IconButton>
-
             </Box>
           </Box>
         </Box>
---
+++ src/components/dashboard/CreateCompany/UploadCompanyBG.jsx
@@ -0,0 +1,109 @@
+import { Box, Button } from "@mui/material";
+
+import { useState, useRef } from "react";
+
+import AvatarEditor from "react-avatar-editor";
+
+import { Dialog, DialogContent, Slider } from "@mui/material";
+
+export default function UploadProfilePhoto({ setPhoto }) {
+  const editorRef = useRef();
+
+  const [image, setImage] = useState(null);
+
+  const [scale, setScale] = useState(1.2);
+
+  const [open, setOpen] = useState(false);
+
+  const [preview, setPreview] = useState("");
+
+  const handleSave = () => {
+    const canvas = editorRef.current.getImageScaledToCanvas();
+
+    canvas.toBlob((blob) => {
+      if (!blob) return;
+
+      const previewUrl = URL.createObjectURL(blob);
+
+      setPreview(previewUrl);
+
+      setPhoto(blob);
+
+      setOpen(false);
+    }, "image/png");
+  };
+
+  return (
+    <>
+      {/* Upload */}
+
+      <Button
+        component="label"
+        size="small"
+        variant="outlined"
+        sx={{
+          borderRadius: "0.4rem",
+          textTransform: "none",
+          borderColor: "#d6d3f0",
+          color: "#5b21b6",
+          fontWeight: 600,
+          fontSize: "0.72rem",
+
+          "&:hover": {
+            borderColor: "#5b21b6",
+            background: "#f8f5ff",
+          },
+        }}
+      >
+        Choose File
+        <input
+          hidden
+          accept="image/*"
+          type="file"
+          onChange={(e) => {
+            const file = e.target.files[0];
+            if (file) {
+              setImage(file);
+              setOpen(true);
+            }
+          }}
+        />
+      </Button>
+
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogContent>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "1rem",
+            }}
+          >
+            <AvatarEditor
+              ref={editorRef}
+              image={image}
+              width={350}
+              height={350}
+              border={20}
+              borderRadius={200}
+              scale={scale}
+            />
+
+            <Slider
+              min={1}
+              max={3}
+              step={0.1}
+              value={scale}
+              onChange={(e, value) => setScale(value)}
+            />
+
+            <Button variant="contained" onClick={handleSave}>
+              Save
+            </Button>
+          </Box>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
