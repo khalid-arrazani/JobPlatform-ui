6a85e0a90838a761aade633d930076cc32d176c0
khalid <karazani2003@gmail.com>
2026-07-06 20:07:22 +0100

    1111

--- src/components/dashboard/Company/EditeCompanyInfo/ModaleCompanyInfo.jsx
+++ src/components/dashboard/Company/EditeCompanyInfo/ModaleCompanyInfo.jsx
@@ -16,12 +16,12 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  Divider
+  Divider,
 } from "@mui/material";
 
 import PersonIcon from "@mui/icons-material/Person";
 import SecurityIcon from "@mui/icons-material/Security";
-import BusinessIcon from '@mui/icons-material/Business';
+import BusinessIcon from "@mui/icons-material/Business";
 import LinkIcon from "@mui/icons-material/Link";
 
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -30,13 +30,23 @@ export default function ModalInfo({ open, setOpen, fetchCompany }) {
   const [reload, setReload] = useState(false);
   const { setSnackBar } = useAuth();
 
-   const [part , setPart]=useState("profile") 
-   
-    const handleProfile = ()=>{setPart("profile")}
-    const handleSecurity = ()=>{setPart("security")}
-    const handleSocial = ()=>{setPart("social")}
-    const handlecompany = ()=>{setPart("company")}
-    const handleDelete = ()=>{setPart("delete")}
+  const [part, setPart] = useState("profile");
+
+  const handleProfile = () => {
+    setPart("profile");
+  };
+  const handleSecurity = () => {
+    setPart("security");
+  };
+  const handleSocial = () => {
+    setPart("social");
+  };
+  const handlecompany = () => {
+    setPart("company");
+  };
+  const handleDelete = () => {
+    setPart("delete");
+  };
   // const DeleteCompanyBanner = async () => {
   //     setReload(true)
   //       try {
@@ -133,106 +143,98 @@ export default function ModalInfo({ open, setOpen, fetchCompany }) {
                 borderRight: "solid 1px #c9c9c9c6",
               }}
             >
-                <List sx={{height:"100%",p:"0.5rem"}}>
+              <List sx={{ height: "100%", p: "0.5rem" }}>
+                {/* Profile Information */}
+                <ListItemButton
+                  onClick={handleProfile}
+                  sx={{
+                    borderRadius: "0.7rem",
+                    mb: 1,
+                    mt: 1,
+                    background: part == "profile" ? "#3131312a" : "none",
+                  }}
+                >
+                  <ListItemIcon>
+                    <PersonIcon sx={{ color: "#4f46e5" }} />
+                  </ListItemIcon>
 
+                  <ListItemText primary="Profile Information" />
+                </ListItemButton>
 
-          {/* Profile Information */}
-          <ListItemButton
-          onClick={handleProfile}
-            sx={{
-              borderRadius: "0.7rem",
-              mb: 1,
-              mt: 1,
-             background :part == 'profile' ? "#3131312a": 'none'
-              
-            }}
-          >
-            <ListItemIcon>
-              <PersonIcon sx={{ color: "#4f46e5" }} />
-            </ListItemIcon>
+                {/* Company Information */}
+                <ListItemButton
+                  onClick={handlecompany}
+                  sx={{
+                    borderRadius: "0.7rem",
+                    mb: 1,
+                    mt: 1,
+                    background: part == "company" ? "#3131312a" : "none",
+                  }}
+                >
+                  <ListItemIcon>
+                    <BusinessIcon sx={{ color: "#4f46e5" }} />
+                  </ListItemIcon>
 
-            <ListItemText primary="Profile Information" />
-          </ListItemButton>
+                  <ListItemText primary="Company Information" />
+                </ListItemButton>
 
-           {/* Company Information */}
-          <ListItemButton
-          onClick={handlecompany}
-            sx={{
-              borderRadius: "0.7rem",
-              mb: 1,
-              mt: 1,
-             background :part == 'company' ? "#3131312a": 'none'
-              
-            }}
-          >
-            <ListItemIcon>
-              <BusinessIcon sx={{ color: "#4f46e5" }} />
-            </ListItemIcon>
+                {/* Security */}
+                <ListItemButton
+                  onClick={handleSecurity}
+                  sx={{
+                    borderRadius: "0.7rem",
+                    mb: 1,
+                    background: part == "security" ? "#3131312a" : "none",
+                  }}
+                >
+                  <ListItemIcon>
+                    <SecurityIcon sx={{ color: "#4f46e5" }} />
+                  </ListItemIcon>
 
-            <ListItemText primary="Company Information" />
-          </ListItemButton>
+                  <ListItemText primary="Security" />
+                </ListItemButton>
 
-          {/* Security */}
-          <ListItemButton
-          onClick={handleSecurity}
+                {/* Social Links */}
+                <ListItemButton
+                  onClick={handleSocial}
+                  sx={{
+                    borderRadius: "0.7rem",
+                    mb: 1,
+                    background: part == "social" ? "#3131312a" : "none",
+                  }}
+                >
+                  <ListItemIcon>
+                    <LinkIcon sx={{ color: "#4f46e5" }} />
+                  </ListItemIcon>
 
-            sx={{
-              borderRadius: "0.7rem",
-              mb: 1,
-              background :part == 'security' ? "#3131312a": 'none'
-            }}
-          >
-            <ListItemIcon>
-              <SecurityIcon sx={{ color: "#4f46e5" }} />
-            </ListItemIcon>
+                  <ListItemText primary="Social Links" />
+                </ListItemButton>
+                
+                <Divider></Divider>
 
-            <ListItemText primary="Security" />
-          </ListItemButton>
+                {/* Delete */}
+                <ListItemButton
+                  onClick={handleDelete}
+                  sx={{
+                    borderRadius: "0.7rem",
 
-          {/* Social Links */}
-          <ListItemButton
-          onClick={handleSocial}
+                    color: "#dc2626",
 
-            sx={{
-              borderRadius: "0.7rem",
-              mb: 1,
-              background :part == 'social' ? "#3131312a": 'none'
+                    "&:hover": {
+                      background: "#fef2f2",
+                    },
+                    position: "absolute",
+                    bottom: "2rem",
+                    background: part == "delete" ? "#e2c9c9d5" : "none",
+                  }}
+                >
+                  <ListItemIcon>
+                    <DeleteIcon sx={{ color: "#dc2626" }} />
+                  </ListItemIcon>
 
-            }}
-          >
-            <ListItemIcon>
-              <LinkIcon sx={{ color: "#4f46e5" }} />
-            </ListItemIcon>
-
-            <ListItemText primary="Social Links" />
-          </ListItemButton>
-          <Divider></Divider>
-         
-          {/* Delete */}
-          <ListItemButton
-          onClick={handleDelete}
-
-            sx={{
-              borderRadius: "0.7rem",
-
-              color: "#dc2626",
-
-              "&:hover": {
-                background: "#fef2f2",
-              },
-              position:"absolute",
-              bottom:"2rem",
-              background :part == 'delete' ? "#e2c9c9d5": 'none'
-
-            }}
-          >
-            <ListItemIcon >
-              <DeleteIcon sx={{ color: "#dc2626" }} />
-            </ListItemIcon>
-
-            <ListItemText primary="Delete Account" />
-          </ListItemButton>
-        </List>
+                  <ListItemText primary="Delete Account" />
+                </ListItemButton>
+              </List>
             </Box>
 
             <Box sx={{ width: "70%", height: "100%" }}></Box>
