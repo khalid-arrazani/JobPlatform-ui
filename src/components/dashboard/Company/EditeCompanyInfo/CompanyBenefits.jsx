69e47f664aee9e1aa049b3f95633765ab5d5b097
khalid <karazani2003@gmail.com>
2026-07-08 20:41:19 +0100

    111

--- src/components/dashboard/Company/EditeCompanyInfo/CompanyBenefits.jsx
+++ src/components/dashboard/Company/EditeCompanyInfo/CompanyBenefits.jsx
@@ -243,7 +243,7 @@ export default function CompanyBenefits({ CompanyInfo,fetchCompany }) {
 
   const [company_benefit, setCompany_benefit] = useState([]);
 
-  console.log(company_benefit);
+  console.log("111",company_benefit);
 
   const selectedBenefitObjects = companyBenefits.filter((benefit) =>
     company_benefit.includes(benefit.label),
