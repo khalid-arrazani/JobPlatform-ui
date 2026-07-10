4527786a5aba4f9446f3589e744a4753947137be
khalid <karazani2003@gmail.com>
2026-07-10 18:55:58 +0100

    1111

--- src/components/dashboard/Company/IfEmpty/EmptyBenefits.jsx
+++ src/components/dashboard/Company/IfEmpty/EmptyBenefits.jsx
@@ -1,17 +1,31 @@
 import { Card, Typography, Box, Button, Collapse } from "@mui/material";
 
-
-
-
-
-
 export default function EmptyBenefits() {
   return (
     <>
-      <Box sx={{height:"10rem",width:"100%",border:"dashed 1px #d0d0d0",borderStyle:"dashed"}} >
-        <Typography sx={{fontWeight:600,fontFamily:"-apple-system",color:"#0b0b14",fontSize:"2rem"}}>Add Benefits </Typography>
-        <Button>Add</Button>
-
+      <Box
+        sx={{
+          height: "10rem",
+          width: "100%",
+          border: "dashed 1px #d0d0d0",
+          borderStyle: "dashed",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: 600,
+            fontFamily: "-apple-system",
+            color: "#0b0b14",
+            fontSize: "2rem",
+          }}
+        >
+          Add Benefits{" "}
+        </Typography>
+        <Button variant="contained">Add</Button>
       </Box>
     </>
   );
