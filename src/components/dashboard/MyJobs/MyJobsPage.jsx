75d20caef61b50feae6515fe56f88c28cefa8d71
khalid <karazani2003@gmail.com>
2026-07-11 10:48:20 +0100

    111

--- src/components/dashboard/MyJobs/MyJobsPage.jsx
+++ src/components/dashboard/MyJobs/MyJobsPage.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box,Typography,Button } from "@mui/material";
 
 
 
@@ -9,6 +9,9 @@ import {
 } from "../../../logic/api/company/Company";
 
 import { useCompany } from "../../../logic/context/CompanyContext";
+
+import AddIcon from '@mui/icons-material/Add';
+
 
 export default function MyJobsPage() {
   const { dispatch, ...state } = useCompany();
@@ -58,7 +61,30 @@ export default function MyJobsPage() {
         }}
       >
         {/* left side */}
-        <Box sx={{width:"75%",height:"100%",bgcolor:"#ddd",borderRadius:"10px 10px 0px 0px"}} ></Box>
+        <Box sx={{width:"75%",height:"100%",bgcolor:"#ffffff",boxSizing: "border-box",overflow:"hidden",borderRadius:"10px 10px 0px 0px",pt:1.5}} >
+           <Box sx={{width:"100%",height:"6rem" ,px:2.5,boxSizing: "border-box",display:"flex",alignContent:"center"}} >
+
+             <Box>
+                <Typography sx={{fontSize:"2rem",fontWeight:600,color:"#050713e7"}} >
+                    My Jobs
+                </Typography>
+                 <Typography sx={{fontSize:"1rem",fontWeight:400,color:"#373737e7"}}>
+                    Manage and track all the job positions you have posted.
+                </Typography>
+             </Box>
+
+
+
+             <Button variant="contained"  sx={{gap:1.5 ,textTransform:"none"}}
+             style={{flexShrink:0}} >
+                <AddIcon/> Post a New Job
+             </Button>
+
+
+             
+           </Box>
+
+        </Box>
 
 
 
