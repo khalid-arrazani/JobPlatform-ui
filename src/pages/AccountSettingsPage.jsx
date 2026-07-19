c70c15fc3617eada405c7fd2b22a8ad0a66d2ee5
khalid <karazani2003@gmail.com>
2026-07-19 20:01:30 +0100

    111

--- src/pages/AccountSettingsPage.jsx
+++ src/pages/AccountSettingsPage.jsx
@@ -1,9 +1,11 @@
 
 import AccountSettingsLayout from "../layouts/AccountSettingsLayout"
 import JobSeekerASPage  from "../components/account-settings/job-seeker/JobSeekerASPage.jsx"
+
 import RecruiterASPage from "../components/account-settings/recruiter/RecruiterASPage.jsx"
+
 export default function AccountSettingsPage() {
-  
+
   return (
     <AccountSettingsLayout>
       <JobSeekerASPage/>
