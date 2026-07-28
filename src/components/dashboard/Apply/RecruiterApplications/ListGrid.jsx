ec2ed0b269a77d96827cd2b9d404a91bb676bf19
khalid <karazani2003@gmail.com>
2026-07-28 20:37:33 +0100

    111

--- src/components/dashboard/Apply/RecruiterApplications/ListGrid.jsx
+++ src/components/dashboard/Apply/RecruiterApplications/ListGrid.jsx
@@ -92,6 +92,8 @@ export default function ListGrid() {
       <Box
         sx={{ height: "100%", width: "100%", px: 1, boxSizing: "border-box" }}
       >
+
+
         <Box
           sx={{
             height: "3.5rem",
@@ -183,6 +185,8 @@ export default function ListGrid() {
             overflow: "auto",
           }}
         >
+
+          
           <Box
             sx={{
               height: "5.5rem",
@@ -190,7 +194,8 @@ export default function ListGrid() {
               boxSizing: "border-box",
               display: "flex",
               px: 2,
-              border: "solid 1px #ddd",
+              
+              borderBottom: "solid 1px #ddd",
             }}
           >
             <Box
@@ -331,6 +336,162 @@ export default function ListGrid() {
 
             </Box>
           </Box>
+          
+          
+          <Box
+            sx={{
+              height: "5.5rem",
+              width: "100%",
+              boxSizing: "border-box",
+              display: "flex",
+              px: 2,
+              
+              borderBottom: "solid 1px #ddd",transition: "0.2s",
+              "&:hover": {
+                transform: "translateY(-2px)",
+                boxShadow: "0 4px 12px rgba(0, 0, 0, 0.05)",
+              },
+            }}
+          >
+            <Box
+              sx={{
+                width: "32%",
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                fontFamily: "system-ui",
+                fontWeight: 600,
+                color: "#02010e9c",
+                fontSize: "0.9rem",
+                gap:1.5
+              }}
+            >
+              <Avatar sx={{ height: "3rem", width: "3rem" }} />
+
+
+              <Box
+              sx={{
+                width: "auto",
+                height: "100%",
+                display: "flex",
+                fontFamily: "system-ui",
+                flexDirection:"column",
+                justifyContent:"center",
+                overflow:"hidden",
+                gap:0.2
+              }}
+            >
+              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"1rem", color:"#020210df"}} >Khalid Arrazani</Typography>
+              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.75rem"}} >Khalid.arrazani@email.com</Typography>
+              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.75rem"}} >+212 777 964 129</Typography>
+              
+            </Box>
+
+
+
+            </Box>
+
+            <Box
+              sx={{
+                width: "20%",
+                height: "100%",
+                display: "flex",
+              
+               
+              
+                flexDirection:"column",
+                justifyContent:"center"
+              }}
+            >
+            
+              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.9rem", color:"#020210df"}} >Frontend Developer</Typography>
+              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.75rem", color:"#6a6969"}} >Full-time • Remote</Typography>
+            </Box>
+
+            <Box
+              sx={{
+                width: "18%",
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                fontFamily: "system-ui",
+                fontWeight: 600,
+                color: "#02010e9c",
+                fontSize: "0.9rem",
+              }}
+            >
+              <Chip
+                  label={`• ${app?.status}`}
+                  sx={{
+                    fontSize: "0.8rem",
+                    alignItems:"center",
+                    borderRadius: "8px",
+                    fontFamily: "system-ui",
+                    color:
+                      app?.status == "Pending"
+                        ? "#F59E0B"
+                        : app?.status == "Accepted"
+                          ? "#10B981"
+                          : app?.status == "Interview"
+                            ? "#8B5CF6"
+                            : app?.status == "Rejected"
+                              ? "#EF4444"
+                              : app?.status == "Under review"
+                                ? "#3B82F6"
+                                : null,
+
+                    bgcolor:
+                      app?.status == "Pending"
+                        ? "#FEF3C7"
+                        : app?.status == "Accepted"
+                          ? "#D1FAE5"
+                          : app?.status == "Interview"
+                            ? "#EDE9FE"
+                            : app?.status == "Rejected"
+                              ? "#FEE2E2"
+                              : app?.status == "Under review"
+                                ? "#DBEAFE"
+                                : null,
+                    fontWeight: 600,
+                  }}
+                />
+            </Box>
+            <Box
+              sx={{
+                width: "13%",
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                fontFamily: "system-ui",
+                fontWeight: 600,
+                color: "#02010e50",
+                fontSize: "0.9rem",
+              }}
+            >
+              May 18,2025
+            </Box>
+            <Box
+              sx={{
+                width: "17%",
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                direction: "rtl",
+                fontFamily: "system-ui",
+                fontWeight: 600,
+                color: "#02010e9c",
+                fontSize: "0.9rem",
+                gap:1
+              }}
+            >
+              
+              <MenuApply JobId={app?.job?._id} />
+
+              <Button sx={{textTransform:"none",fontSize:"0.8rem",color:"#000000c3" , border:"#ddd 1px solid",fontWeight:600,fontFamily:"system-ui"}}  >View Profile</Button>
+
+            </Box>
+          </Box>
+
         </Box>
       </Box>
     </>
