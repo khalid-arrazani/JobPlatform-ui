e139bbafe4bb0de3cc967a9862e986e1fc14093d
khalid <karazani2003@gmail.com>
2026-07-28 19:40:38 +0100

    jj

--- src/components/dashboard/Apply/RecruiterApplications/ListGrid.jsx
+++ src/components/dashboard/Apply/RecruiterApplications/ListGrid.jsx
@@ -241,7 +241,7 @@ export default function ListGrid() {
             >
             
               <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.9rem", color:"#020210df"}} >Frontend Developer</Typography>
-              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.75rem", color:"#6a6969"}} >Khalid.arrazani@email.com</Typography>
+              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.75rem", color:"#6a6969"}} >Full-time • Remote</Typography>
             </Box>
 
             <Box
