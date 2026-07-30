e17afd5e677bba6ee551858b8cbea54cb7c57238
khalid <karazani2003@gmail.com>
2026-07-30 20:26:51 +0100

    1111

--- src/components/dashboard/Apply/RecruiterApplications/ListGrid.jsx
+++ src/components/dashboard/Apply/RecruiterApplications/ListGrid.jsx
@@ -12,7 +12,8 @@ import {
   Box,
   Typography,
   Container,
-  Chip,Button
+  Chip,
+  Button,
 } from "@mui/material";
 import MenuApply from "./Menu";
 import { useApply } from "../../../../logic/context/ApplyContext";
@@ -83,28 +84,22 @@ const applications = [
   },
 ];
 import { formatDistanceToNow, format } from "date-fns";
-const app={status :"Under review"}
-
+const app = { status: "Under review" };
 
 export default function ListGrid() {
-
- const {...state } = useApply();
+  const { ...state } = useApply();
 
   const handleClick = (application) => {
     console.log(application);
   };
 
- console.log("....",state);
-
-
+  console.log("....", state);
 
   return (
     <>
       <Box
         sx={{ height: "100%", width: "100%", px: 1, boxSizing: "border-box" }}
       >
- 
-   
         <Box
           sx={{
             height: "3.5rem",
@@ -187,11 +182,6 @@ export default function ListGrid() {
           </Box>
         </Box>
 
-
-
-
-      
-
         <Box
           sx={{
             height: "83%",
@@ -201,97 +191,137 @@ export default function ListGrid() {
             overflow: "auto",
           }}
         >
-
-         {state?.ListApply?.applications?.map((apply)=>(
-          <Box
-          key={apply._id}
-            sx={{
-              height: "5.5rem",
-              width: "100%",
-              boxSizing: "border-box",
-              display: "flex",
-              px: 2,
-              
-              borderBottom: "solid 1px #ddd", transition: "0.2s",
-              "&:hover": {
-                transform: "translateY(-2px)",
-                boxShadow: "0 4px 12px rgba(0, 0, 0, 0.05)",
-                bgcolor:"#f6fbff"
-              },
-            }}
-          >
+          {state?.ListApply?.applications?.map((apply) => (
             <Box
+              key={apply._id}
               sx={{
-                width: "32%",
-                height: "100%",
+                height: "5.5rem",
+                width: "100%",
+                boxSizing: "border-box",
                 display: "flex",
-                alignItems: "center",
-                fontFamily: "system-ui",
-                fontWeight: 600,
-                color: "#02010e9c",
-                fontSize: "0.9rem",
-                gap:1.5
+                px: 2,
+
+                borderBottom: "solid 1px #ddd",
+                transition: "0.2s",
+                "&:hover": {
+                  transform: "translateY(-2px)",
+                  boxShadow: "0 4px 12px rgba(0, 0, 0, 0.05)",
+                  bgcolor: "#f6fbff",
+                },
               }}
             >
-              <Avatar src={apply.profile.ProfileImage.url} sx={{ height: "3rem", width: "3rem" }}/>
+              <Box
+                sx={{
+                  width: "32%",
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  fontFamily: "system-ui",
+                  fontWeight: 600,
+                  color: "#02010e9c",
+                  fontSize: "0.9rem",
+                  gap: 1.5,
+                }}
+              >
+                <Avatar
+                  src={apply.profile.ProfileImage.url}
+                  sx={{ height: "3rem", width: "3rem" }}
+                />
 
+                <Box
+                  sx={{
+                    width: "auto",
+                    height: "100%",
+                    display: "flex",
+                    fontFamily: "system-ui",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    overflow: "hidden",
+                    gap: 0.2,
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontFamily: "system-ui",
+                      fontWeight: 600,
+                      fontSize: "1rem",
+                      color: "#020210df",
+                    }}
+                  >
+                    {apply.profile.fullName}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      fontFamily: "system-ui",
+                      fontWeight: 600,
+                      fontSize: "0.75rem",
+                    }}
+                  >
+                    {apply.applicant.email}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      fontFamily: "system-ui",
+                      fontWeight: 600,
+                      fontSize: "0.75rem",
+                    }}
+                  >
+                    {apply.profile.location.slice(0, 20)}
+                    {apply.profile.location.length > 20 ? "....." : null}{" "}
+                  </Typography>
+                </Box>
+              </Box>
 
               <Box
-              sx={{
-                width: "auto",
-                height: "100%",
-                display: "flex",
-                fontFamily: "system-ui",
-                flexDirection:"column",
-                justifyContent:"center",
-                overflow:"hidden",
-                gap:0.2
-              }}
-            >
-              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"1rem", color:"#020210df"}} >{apply.profile.fullName}</Typography>
-              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.75rem"}} >{apply.applicant.email}</Typography>
-              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.75rem"}} >{apply.profile.location.slice(0,20) }{apply.profile.location.length > 20 ? '.....' : null}  </Typography>
-              
-            </Box>
+                sx={{
+                  width: "20%",
+                  height: "100%",
+                  display: "flex",
 
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontFamily: "system-ui",
+                    fontWeight: 600,
+                    fontSize: "0.9rem",
+                    color: "#020210df",
+                  }}
+                >
+                  {apply.job.title.slice(0, 26)}
+                  {apply.job.title.length > 26 ? "....." : null}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontFamily: "system-ui",
+                    fontWeight: 600,
+                    fontSize: "0.75rem",
+                    color: "#6a6969",
+                  }}
+                >
+                  {apply.job.jobType} • {apply.job.workMode}
+                </Typography>
+              </Box>
 
-
-            </Box>
-
-            <Box
-              sx={{
-                width: "20%",
-                height: "100%",
-                display: "flex",
-              
-               
-              
-                flexDirection:"column",
-                justifyContent:"center"
-              }}
-            >
-            
-              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.9rem", color:"#020210df"}} >{apply.job.title.slice(0,26)}{apply.job.title.length > 26 ? '.....' : null}</Typography>
-              <Typography sx={{fontFamily: "system-ui",fontWeight:600,fontSize:"0.75rem", color:"#6a6969"}} >{apply.job.jobType} • {apply.job.workMode}</Typography>
-            </Box>
-
-            <Box
-              sx={{
-                width: "18%",
-                height: "100%",
-                display: "flex",
-                alignItems: "center",
-                fontFamily: "system-ui",
-                fontWeight: 600,
-                color: "#02010e9c",
-                fontSize: "0.9rem",
-              }}
-            >
-              <Chip
+              <Box
+                sx={{
+                  width: "18%",
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  fontFamily: "system-ui",
+                  fontWeight: 600,
+                  color: "#02010e9c",
+                  fontSize: "0.9rem",
+                }}
+              >
+                <Chip
                   label={`• ${apply.status}`}
                   sx={{
                     fontSize: "0.8rem",
-                    alignItems:"center",
+                    alignItems: "center",
                     borderRadius: "8px",
                     fontFamily: "system-ui",
                     color:
@@ -322,60 +352,56 @@ export default function ListGrid() {
                     fontWeight: 600,
                   }}
                 />
+              </Box>
+              <Box
+                sx={{
+                  width: "13%",
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  fontFamily: "system-ui",
+                  fontWeight: 600,
+                  color: "#02010e50",
+                  fontSize: "0.9rem",
+                }}
+              >
+                {format(new Date(apply?.createdAt), "MMM dd, yyyy")}
+              </Box>
+              <Box
+                sx={{
+                  width: "17%",
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  direction: "rtl",
+                  fontFamily: "system-ui",
+                  fontWeight: 600,
+                  color: "#02010e9c",
+                  fontSize: "0.9rem",
+                  gap: 1,
+                }}
+              >
+                <MenuApply JobId={apply?.job?._id} />
+
+                <Button
+                  sx={{
+                    textTransform: "none",
+                    fontSize: "0.8rem",
+                    color: "#000000c3",
+                    border: "#ddd 1px solid",
+                    fontWeight: 600,
+                    fontFamily: "system-ui",
+                    bgcolor: "#fffefe",
+                  }}
+                >
+                  View Profile
+                </Button>
+
+
+              </Box>
             </Box>
-            <Box
-              sx={{
-                width: "13%",
-                height: "100%",
-                display: "flex",
-                alignItems: "center",
-                fontFamily: "system-ui",
-                fontWeight: 600,
-                color: "#02010e50",
-                fontSize: "0.9rem",
-              }}
-            >
-              {format(new Date(apply?.createdAt), "MMM dd, yyyy")}
-            </Box>
-            <Box
-              sx={{
-                width: "17%",
-                height: "100%",
-                display: "flex",
-                alignItems: "center",
-                direction: "rtl",
-                fontFamily: "system-ui",
-                fontWeight: 600,
-                color: "#02010e9c",
-                fontSize: "0.9rem",
-                gap:1
-              }}
-            >
-              
-              <MenuApply JobId={app?.job?._id} />
-
-              <Button sx={{textTransform:"none",fontSize:"0.8rem",color:"#000000c3" , border:"#ddd 1px solid",fontWeight:600,fontFamily:"system-ui",bgcolor:"#fffefe"}}  >View Profile</Button>
-
-            </Box>
-          </Box>
-          
-          
-         
-
-
-          ))
-}
-
+          ))}
         </Box>
-
-
- 
-
-
-
-
-
-
       </Box>
     </>
   );
