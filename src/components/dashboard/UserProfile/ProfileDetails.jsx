30bdb4da02f6c7301f1f35290000b895211b4fd3
khalid <karazani2003@gmail.com>
2026-08-01 22:07:04 +0100

    11

--- src/components/dashboard/UserProfile/ProfileDetails.jsx
+++ src/components/dashboard/UserProfile/ProfileDetails.jsx
@@ -1,12 +1,14 @@
 
 // import RecruiterProfilePage from "./RecruiterProfile/RecruiterProfilePage"
 
+import { useParams } from "react-router-dom";
+
 // import MyProfilePage from "./JobSeekerProfile/JobSeekerProfilePage"
 
 export default function ProfileDetails (){
 
-    
-
+    const { ProfileId , Role } = useParams();
+    console.log(ProfileId , Role );
     return <>
      
     </>
