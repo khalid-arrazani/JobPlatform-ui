34002e41010d4adc8cc046507e87a67ba5385828
khalid <karazani2003@gmail.com>
2026-08-04 20:38:19 +0100

    1111

--- src/components/dashboard/Apply/RecruiterApplications/ListGrid.jsx
+++ src/components/dashboard/Apply/RecruiterApplications/ListGrid.jsx
@@ -25,13 +25,7 @@ export default function ListGrid() {
     <>
       {state?.ListApply?.hasApply ?  <Box
         sx={{ height: "100%", width: "100%", px: 1, boxSizing: "border-box" }}
-      >
-
-
-
-
-
-        
+      >   
         <Box
           sx={{
             height: "3.5rem",
@@ -125,7 +119,6 @@ export default function ListGrid() {
             overflow: "auto",
           }}
         >
-          
            {state?.ListApply?.applications?.map((apply) => (
             <Box
               key={apply._id}
