b5b4e95c883eb80697eb70213d96d3b98d8171fa
khalid <karazani2003@gmail.com>
2026-08-05 20:48:10 +0100

    1111

--- src/components/dashboard/Apply/RecruiterApplications/ApplicationsPage.jsx
+++ src/components/dashboard/Apply/RecruiterApplications/ApplicationsPage.jsx
@@ -1,4 +1,4 @@
-import { Box,Avatar ,Typography, Chip} from "@mui/material";
+import { Box, Avatar, Typography, Chip } from "@mui/material";
 
 import { Pagination } from "@mui/material";
 
@@ -20,7 +20,7 @@ export default function ApplicationsRc() {
     ApplyJobs();
   }, []);
 
-  const apply = {status:"Pending"}
+  const apply = { status: "Pending" };
 
   return (
     <>
@@ -80,72 +80,156 @@ export default function ApplicationsRc() {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            boxSizing:"border-box",
-            p:2
+            boxSizing: "border-box",
+            p: 2,
           }}
         >
-         <Box sx={{boxSizing:"border-box",border:"1px #ddd dashed",height:"100%",width:"100%", borderRadius:"15px",px:1}}>
+          <Box
+            sx={{
+              boxSizing: "border-box",
+              border: "1px #ddd dashed",
+              height: "100%",
+              width: "100%",
+              borderRadius: "15px",
+              px: 1,
+            }}
+          >
+            <Box
+              sx={{
+                height: "40%",
+                width: "100%",
+                borderBottom: "1px dashed #ddd",
+                placeItems: "center",
+                boxSizing: "border-box",
+                p: 2,
+              }}
+            >
+              <Avatar sx={{ height: "8rem", width: "8rem" }} />
 
+              <Typography
+                sx={{
+                  fontSize: "1.6rem",
+                  fontFamily: "system-ui",
+                  fontWeight: 600,
+                  color: "#00010ed0",
+                  bt: 1,
+                }}
+              >
+                {" "}
+                Khalid Arazani{" "}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "0.9rem",
+                  fontFamily: "monospace",
+                  fontWeight: 600,
+                  color: "#00010eac",
+                  bt: 1,
+                }}
+              >
+                {" "}
+                Frontend React Developer{" "}
+              </Typography>
+            </Box>
 
-
-             <Box sx={{height:"40%",width:"100%",borderBottom:"1px dashed #ddd",placeItems:"center",boxSizing:"border-box",p:2}} >
-
-              <Avatar sx={{height:"8rem",width:"8rem"}}/>
-
-              <Typography sx={{fontSize:"1.6rem",fontFamily:"system-ui",fontWeight:600,color:"#00010ed0",bt:1}} > Khalid Arazani </Typography>
-              <Typography sx={{fontSize:"0.9rem",fontFamily:"monospace",fontWeight:600,color:"#00010eac",bt:1}} > Frontend React Developer </Typography>
-             </Box>
-
-             <Box sx={{height:"60%",width:"100%",boxSizing:"border-box",p:2,display:"flex",flexDirection:"column",alignContent:"center",gap:2}}>
-
+            <Box
+              sx={{
+                height: "60%",
+                width: "100%",
+                boxSizing: "border-box",
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                alignContent: "center",
+                gap: 2,
+              }}
+            >
               <Chip
-                  label={`• ${apply?.status}`}
-                  sx={{
-                    fontSize: "1.2rem",
-                    alignItems: "center",
-                    borderRadius: "8px",
-                    fontFamily: "monospace",
-                    color:
-                      apply?.status == "Pending"
-                        ? "#F59E0B"
-                        : apply?.status == "Accepted"
-                          ? "#10B981"
-                          : apply?.status == "Interview"
-                            ? "#8B5CF6"
-                            : apply?.status == "Rejected"
-                              ? "#EF4444"
-                              : apply?.status == "Under review"
-                                ? "#3B82F6"
-                                : null,
+                label={`• ${apply?.status}`}
+                sx={{
+                  fontSize: "1.2rem",
+                  alignItems: "center",
+                  borderRadius: "8px",
+                  fontFamily: "monospace",
+                  color:
+                    apply?.status == "Pending"
+                      ? "#F59E0B"
+                      : apply?.status == "Accepted"
+                        ? "#10B981"
+                        : apply?.status == "Interview"
+                          ? "#8B5CF6"
+                          : apply?.status == "Rejected"
+                            ? "#EF4444"
+                            : apply?.status == "Under review"
+                              ? "#3B82F6"
+                              : null,
 
-                    bgcolor:
-                      apply?.status == "Pending"
-                        ? "#FEF3C7"
-                        : apply?.status == "Accepted"
-                          ? "#D1FAE5"
-                          : apply?.status == "Interview"
-                            ? "#8a5cf620 "
-                            : apply?.status == "Rejected"
-                              ? "#FEE2E2"
-                              : apply?.status == "Under review"
-                                ? "#DBEAFE"
-                                : null,
-                    fontWeight: 600,
-                    mb:2
-                  }}
+                  bgcolor:
+                    apply?.status == "Pending"
+                      ? "#FEF3C7"
+                      : apply?.status == "Accepted"
+                        ? "#D1FAE5"
+                        : apply?.status == "Interview"
+                          ? "#8a5cf620 "
+                          : apply?.status == "Rejected"
+                            ? "#FEE2E2"
+                            : apply?.status == "Under review"
+                              ? "#DBEAFE"
+                              : null,
+                  fontWeight: 600,
+                  mb: 2,
+                }}
               />
-              <Typography sx={{fontSize:"1rem",fontFamily:"monospace",fontWeight:600,color:"#00010eac",bt:1}} > 📍 Taroudant, Morocco  </Typography>
-              <Typography sx={{fontSize:"1rem",fontFamily:"monospace",fontWeight:600,color:"#00010eac",bt:1}} > ✉ karazani2003@gmail.com </Typography>
-              <Typography sx={{fontSize:"1rem",fontFamily:"monospace",fontWeight:600,color:"#00010eac",bt:1}} > 📞 +212 777964129   </Typography>
-              
-
-              
-             </Box>
-
-
-
-         </Box>
-          
+              <Typography
+                sx={{
+                  fontSize: "1rem",
+                  fontFamily: "monospace",
+                  fontWeight: 600,
+                  color: "#00010eac",
+                  bt: 1,
+                }}
+              >
+                {" "}
+                📍 Taroudant, Morocco{" "}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "1rem",
+                  fontFamily: "monospace",
+                  fontWeight: 600,
+                  color: "#00010eac",
+                  bt: 1,
+                }}
+              >
+                {" "}
+                ✉ karazani2003@gmail.com{" "}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "1rem",
+                  fontFamily: "monospace",
+                  fontWeight: 600,
+                  color: "#00010eac",
+                  bt: 1,
+                }}
+              >
+                {" "}
+                📞 +212 777964129{" "}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "0.8rem",
+                  fontFamily: "monospace",
+                  fontWeight: 600,
+                  color: "#00010eac",
+                  bt: 1,
+                }}
+              >
+                
+                ✔ React - ✔ JavaScript - ✔ Node.js
+              </Typography>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </>
