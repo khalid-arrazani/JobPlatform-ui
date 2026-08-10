5bb5b97c915f4222d9d108c60b3d6b5bc6148c28
khalid <karazani2003@gmail.com>
2026-08-10 19:57:04 +0100

    1111

--- src/pages/CompleteProfilePage.jsx
+++ src/pages/CompleteProfilePage.jsx
@@ -8,10 +8,10 @@ import { useAuth } from "../logic/context/AuthContext.jsx";
 
 export default function CompleteProfilePage() {
 
-  const {  ...state} = useAuth()
+  const {...state} = useAuth()
 
 
-console.log(state,5555);
+console.log(state , 5555);
 
 
   return (
