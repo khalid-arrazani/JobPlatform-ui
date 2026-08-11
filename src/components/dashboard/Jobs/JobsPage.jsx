bb56eeedc28adb05c3bd1d24da66276b3b926a6a
khalid <karazani2003@gmail.com>
2026-08-11 20:32:28 +0100

    vv

--- src/components/dashboard/Jobs/JobsPage.jsx
+++ src/components/dashboard/Jobs/JobsPage.jsx
@@ -21,13 +21,20 @@ import LoadingList from "./LoadingList.jsx";
 import { useProfile } from "../../../logic/context/profileContext.jsx";
 
 export default function JobsPage() {
-  const { dispatch : dispatch1, ...state1 } = useProfile();
+  const {fetchUser, dispatch : dispatch1, ...state1 } = useProfile();
   const { dispatch, ...state } = useJob();
 
 
 
   const { setSnackBar } = useAuth();
   const [page, setPage] = useState(1);
+
+
+
+
+
+
+
 
   const handleChange = (event, value) => {
     setPage(value);
