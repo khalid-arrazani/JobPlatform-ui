b91c65ee5dd25cb779746b171c48c139593d9ed3
khalid <karazani2003@gmail.com>
2026-08-14 00:10:05 +0100

    111

--- src/components/dashboard/Company/CompanyPage.jsx
+++ src/components/dashboard/Company/CompanyPage.jsx
@@ -1,5 +1,8 @@
 import { Box } from "@mui/material";
 
+import Lottie from "lottie-react";
+import loadingAnimation from "../../../assets/system-outline-4018-spinner-three-dots-hover-trapdoor.json";
+
 import CompanyHeader from "./CompanyHeader";
 import AboutCompany from "./AboutCompany";
 import OpenPositionsCard from "./OpenPositionsCard ";
@@ -8,26 +11,25 @@ import CompanyStatistics from "./CompanyStatistics";
 import CompanyBenefits from "./CompanyBenefits ";
 import SocialContact from "./SocialContact";
 import CompanyInfo from "./CompanyInfo";
-import { useEffect } from "react";
-import {
-  getmyCompany,
-} from "../../../logic/api/company/Company";
+import { useEffect, useState } from "react";
+import { getmyCompany } from "../../../logic/api/company/Company";
 
 import { useCompany } from "../../../logic/context/CompanyContext";
 
 export default function CompanyPage() {
   const { dispatch, ...state } = useCompany();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchCompany();
   }, []);
 
   const fetchCompany = async () => {
-
     dispatch({
       type: "SET_LOADING",
       payload: true,
     });
+    setLoading(true);
 
     try {
       const data = await getmyCompany();
@@ -35,89 +37,105 @@ export default function CompanyPage() {
         type: "SETCOMPANY",
         payload: data,
       });
-
-
     } catch (error) {
-
       console.log(error.response?.data);
-
     } finally {
- 
       dispatch({
         type: "SET_LOADING",
         payload: false,
       });
+      setLoading(false);
     }
   };
 
   return (
     <>
-      <Box
-        sx={{
-          height: "auto",
-          width: "100vw",
-          bgcolor: "#f2f2f5",
-          boxSizing: "border-box",
-          px: 6,
-          pt: 3,
-          overflow: "auto",
-        }}
-      >
-        {/* top side or Company Card or  Header   */}
-        <CompanyHeader
-          CompanyInfo={state.Company?.company}
-          fetchCompany={fetchCompany}
-        />
-
-        {/* Content about Company or bottom side  */}
+      {loading ? (
         <Box
           sx={{
-            width: "100%",
-            height: "fit-Content",
-            my: 2,
-            borderRadius: "15px",
-            display: "flex",
-            gap: 1.5,
+            height: "auto",
+            width: "100vw",
+            bgcolor: "#f2f2f5",
+            boxSizing: "border-box",
+            px: 6,
+            pt: 3,
+            overflow: "auto",
           }}
         >
-          {/* left side  */}
+          <Lottie
+            animationData={loadingAnimation}
+            loop={true}
+            style={{ width: "100%", height: "100%" }}
+          />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            height: "auto",
+            width: "100vw",
+            bgcolor: "#f2f2f5",
+            boxSizing: "border-box",
+            px: 6,
+            pt: 3,
+            overflow: "auto",
+          }}
+        >
+          {/* top side or Company Card or  Header   */}
+          <CompanyHeader
+            CompanyInfo={state.Company?.company}
+            fetchCompany={fetchCompany}
+          />
+
+          {/* Content about Company or bottom side  */}
           <Box
             sx={{
-              width: "50%",
-              borderRadius: "15px",
-            }}
-          >
-            {/* About Company Card  */}
-            <AboutCompany CompanyInfo={state.Company?.company} />
-            {/* Company Statistics  */}
-            <CompanyStatistics CompanyInfo={state?.Company} />
-
-            {/* Open Positions Card  */}
-            <OpenPositionsCard
-              CompanyInfo={state?.Company}
-              CompanyactiveJobs={state?.Company?.activeJobs}
-            />
-          </Box>
-
-          {/* right side  */}
-          <Box
-            sx={{
-              width: "50%",
+              width: "100%",
               height: "fit-Content",
+              my: 2,
               borderRadius: "15px",
+              display: "flex",
+              gap: 1.5,
             }}
           >
-            {/* Company Benefits Card  */}
-            <SocialContact CompanyInfo={state.Company?.company} />
+            {/* left side  */}
+            <Box
+              sx={{
+                width: "50%",
+                borderRadius: "15px",
+              }}
+            >
+              {/* About Company Card  */}
+              <AboutCompany CompanyInfo={state.Company?.company} />
+              {/* Company Statistics  */}
+              <CompanyStatistics CompanyInfo={state?.Company} />
 
-            {/* Company Benefits Card  */}
-            <CompanyBenefits CompanyInfo={state.Company?.company} />
+              {/* Open Positions Card  */}
+              <OpenPositionsCard
+                CompanyInfo={state?.Company}
+                CompanyactiveJobs={state?.Company?.activeJobs}
+              />
+            </Box>
 
-            {/* Company Info Card  */}
-            <CompanyInfo CompanyInfo={state.Company?.company} />
+            {/* right side  */}
+            <Box
+              sx={{
+                width: "50%",
+                height: "fit-Content",
+                borderRadius: "15px",
+              }}
+            >
+              {/* Company Benefits Card  */}
+              <SocialContact CompanyInfo={state.Company?.company} />
+
+              {/* Company Benefits Card  */}
+              <CompanyBenefits CompanyInfo={state.Company?.company} />
+
+              {/* Company Info Card  */}
+              <CompanyInfo CompanyInfo={state.Company?.company} />
+            </Box>
           </Box>
         </Box>
-      </Box>
+      )}
     </>
   );
 }
