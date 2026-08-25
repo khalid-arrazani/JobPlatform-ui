024f84744f0dd1d84d149974026cf2f556de60a9
khalid <karazani2003@gmail.com>
2026-08-25 19:45:14 +0100

    ddd

---
+++ src/components/dashboard/CompanyDetails/OpenPositionsModal/Modal.jsx
@@ -0,0 +1,43 @@
+import { Typography, Box, IconButton, Modal } from "@mui/material";
+import { useState } from "react";
+
+
+
+
+
+export default function ModalOpenPositions ( ){
+  const  [open , setOpen ] = useState(false)
+
+ const handleClose = ()=>{
+    setOpen(false)
+  }
+
+
+
+    return <>
+     <Modal
+        open={open}
+        onClose={handleClose}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            height: "95%",
+            width: "95%",
+            bgcolor: "#ffffff",
+            borderRadius: "20px",
+            overflow: "hidden",
+            boxSizing: "border-box",
+            outline: "none",
+          }}
+        >
+          
+        </Box>
+      </Modal>
+    
+    </>
+}
