de3e68a019f238fe5a0318b0d9e433821fdac87b
khalid <karazani2003@gmail.com>
2026-09-05 20:33:20 +0100

    111

--- src/pages/AccountSettingsPage.jsx
+++ src/pages/AccountSettingsPage.jsx
@@ -1,14 +1,15 @@
 
 import AccountSettingsLayout from "../layouts/AccountSettingsLayout"
 import JobSeekerASPage  from "../components/account-settings/job-seeker/JobSeekerASPage.jsx"
-
+import { useAuth } from "../logic/context/AuthContext.jsx";
 import RecruiterASPage from "../components/account-settings/recruiter/RecruiterASPage.jsx"
 
 export default function AccountSettingsPage() {
-
+const {...state} = useAuth()
   return (
     <AccountSettingsLayout>
-      <RecruiterASPage/>
+   
+       {state.user?.user?.role == "jobSeeker" ? <JobSeekerASPage/>  : state.user?.user?.role == "recruiter" ? <RecruiterASPage/> : null }
     </AccountSettingsLayout>
   );
 }
