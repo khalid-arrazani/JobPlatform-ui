6af785426e7f5a445682a45e9d148c0d6b1495c1
khalid <karazani2003@gmail.com>
2026-09-07 18:58:59 +0100

    11

--- src/components/auth/forMobile/SignInForm.jsx
+++ src/components/auth/forMobile/SignInForm.jsx
@@ -20,7 +20,9 @@ export default function SignInMobile() {
             flexDirection:"column-reverse",
         }}
       >
-        <Box sx={{height:"75%" , width:'100%',bgcolor:"#fff",borderTopLeftRadius:"100% 20% ",}}  style={{CornerRightUp:""}}  ></Box>
+        <Box sx={{height:"75%" , width:'100%',bgcolor:"#fff",borderTopLeftRadius:"100% 20% ",}}  style={{CornerRightUp:""}}  >
+          
+        </Box>
       </Box>
     </>
   );
