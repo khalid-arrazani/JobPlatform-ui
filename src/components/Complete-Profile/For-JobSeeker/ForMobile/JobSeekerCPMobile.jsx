5a329f96f6aea5f80027269c0717073c10e19674
khalid <karazani2003@gmail.com>
2026-09-14 21:52:00 +0100

    1111

---
+++ src/components/Complete-Profile/For-JobSeeker/ForMobile/JobSeekerCPMobile.jsx
@@ -0,0 +1,33 @@
+import { Box, Typography, Chip, Button, TextField } from "@mui/material";
+
+export default function JobSeekerCPMobile() {
+  return (
+    <>
+      <Box
+        sx={{
+          height: "100%",
+          width: "100vw",
+          background:
+            "linear-gradient( 135deg,#5a21b623 0%, #6c28d92f 50%, #7c3aed94 100%)",display:"flex",justifyContent:"center",alignItems:"center"
+            
+        }}
+      >
+        <Box sx={{
+          height: "95%",
+          width: "97%",
+          background:
+            "#fafafa",
+            borderRadius:"20px",
+            border:"solid 1px #837f834b",
+            
+        }}>
+
+          <Box sx={{width:"100%",
+          }}></Box>
+
+        </Box>
+
+      </Box>
+    </>
+  );
+}
--- src/components/Complete-Profile/For-JobSeeker/JobSeekerCPPage.jsx
+++ src/components/Complete-Profile/For-JobSeeker/JobSeekerCPPage.jsx
@@ -1,14 +1,18 @@
+import JobSeekerCPMobile from "./ForMobile/JobSeekerCPMobile";
 import JobSeekerCPPc from "./ForPc/JobSeekerCPPc";
 
 
-
-
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function JobSeekerCPPage() {
-  
+    const isMobile = useMediaQuery("(max-width:600px)");
+   const isTablet = useMediaQuery("(min-width:601px) and (max-width:1024px)");
   return (
     <>
-     <JobSeekerCPPc/>
+ 
+     {isMobile && <JobSeekerCPMobile />}
+     
+     {!isMobile && <JobSeekerCPPc />}
     </>
   );
 }
