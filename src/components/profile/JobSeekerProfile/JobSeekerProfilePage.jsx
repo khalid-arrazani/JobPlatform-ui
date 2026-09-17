ebb1cd22b2d2a07be37581b128002ce527d44505
khalid <karazani2003@gmail.com>
2026-09-17 10:24:58 +0100

    1111

--- src/components/profile/JobSeekerProfile/JobSeekerProfilePage.jsx
+++ src/components/profile/JobSeekerProfile/JobSeekerProfilePage.jsx
@@ -1,5 +1,5 @@
 import JobSeekerProfileDesktop from "./Desktop/JobSeekerProfiledesktop";
-
+import JobSeekerProfileMobile from "./Mobile/JobSeekerProfileMobile";
 
 export default function MyProfileJS() {
 
