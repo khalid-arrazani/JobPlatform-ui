a4dbfb4cb7eadd77e96287ab8baf1cfd9dc48247
khalid <karazani2003@gmail.com>
2026-09-19 10:15:57 +0100

    1111

--- src/components/profile/JobSeekerProfile/Mobile/JobSeekerProfileMobile.jsx
+++ src/components/profile/JobSeekerProfile/Mobile/JobSeekerProfileMobile.jsx
@@ -159,6 +159,7 @@ export default function JobSeekerProfileMobile() {
 
           {/* lower */}
           <Box sx={{ width: "100%", boxSizing: "border-box", px: 2 }}>
+            
             {/* Info */}
             <Box
               sx={{
@@ -203,8 +204,7 @@ export default function JobSeekerProfileMobile() {
               </Typography>
             </Box>
 
-            
-
+            {/* Experience */}
             <Paper
               elevation={0}
               sx={{
@@ -356,8 +356,7 @@ export default function JobSeekerProfileMobile() {
               </Box>
             </Paper>
 
-
-
+            {/* Education */}
             <Paper
               elevation={0}
               sx={{
@@ -563,10 +562,7 @@ export default function JobSeekerProfileMobile() {
               </Box>
             </Paper>
 
-
-
-
-
+            {/* Social Links */}
             <Paper
               elevation={0}
               sx={{
@@ -576,7 +572,7 @@ export default function JobSeekerProfileMobile() {
                 width: "100%",
 
                 boxSizing: "border-box",
-                border: "1px dashed #ddd",
+                border: "1px dashed #ddd",mb:3
               }}
             >
               <Box
@@ -606,15 +602,34 @@ export default function JobSeekerProfileMobile() {
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
-                      viewBox="0 0 24 24"
+                      viewBox="0 0 14 14"
+                      id="Link-Chain--Streamline-Flex"
                       height="24"
                       width="24"
                     >
-                      <g id="motion-photos-on">
+                      <g id="link-chain--create-hyperlink-link-make-unlink-connection-chain">
                         <path
-                          id="Union"
-                          fill="#000000c8"
-                          d="M12 2c5.5228 0 10 4.47715 10 10 0 5.5228 -4.4772 10 -10 10 -5.52285 0 -10 -4.4772 -10 -10 0 -1.3775 0.27949 -2.69283 0.78516 -3.88965l1.84179 0.7793C4.22359 9.84449 4 10.8948 4 12c0 4.4183 3.58172 8 8 8 4.4183 0 8 -3.5817 8 -8 0 -4.41828 -3.5817 -8 -8 -8 -1.1052 0 -2.15551 0.22359 -3.11035 0.62695l-0.7793 -1.84179C9.30717 2.27949 10.6225 2 12 2M5.5 4C6.32843 4 7 4.67157 7 5.5S6.32843 7 5.5 7 4 6.32843 4 5.5 4.67157 4 5.5 4"
+                          id="Vector"
+                          stroke="#000000c8"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M8.858 5.143 5.143 8.857"
+                          stroke-width="1"
+                        ></path>
+                        <path
+                          id="Vector 2470"
+                          stroke="#000000c8"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M8.235 11.343c-2.353 2.073 -4.535 3.094 -6.603 1.025 -2.051 -2.05 -1.065 -4.212 0.972 -6.542"
+                          stroke-width="1"
+                        ></path>
+                        <path
+                          id="Vector 2471"
+                          stroke="#000000c8"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M5.766 2.657C8.118 0.584 10.3 -0.437 12.369 1.632c2.05 2.05 1.064 4.212 -0.973 6.542"
                           stroke-width="1"
                         ></path>
                       </g>
@@ -625,7 +640,7 @@ export default function JobSeekerProfileMobile() {
                     variant="h6"
                     sx={{ fontFamily: "system-ui", color: "#0b0317d8" }}
                   >
-                    Experience
+                    Social Links
                   </Typography>
                 </Box>
 
@@ -682,7 +697,7 @@ export default function JobSeekerProfileMobile() {
                   variant="h5"
                   sx={{ color: "#000000af", fontFamily: "monospace", mb: 0.6 }}
                 >
-                  No experience added yet
+                  No social links added yet
                 </Typography>
 
                 <Typography
@@ -695,8 +710,8 @@ export default function JobSeekerProfileMobile() {
                     fontSize: "0.8rem",
                   }}
                 >
-                  Add your work experience to highlight your professional
-                  journey.
+                  Add your LinkedIn, GitHub, portfolio, Twitter, or other
+                  professional links to help employers learn more about you.
                 </Typography>
 
                 <Button
@@ -712,10 +727,155 @@ export default function JobSeekerProfileMobile() {
                     borderRadius: "15px",
                   }}
                 >
-                  Add Experience
+                  Add Link
                 </Button>
               </Box>
             </Paper>
+
+            {/* Skills */}
+            <Paper
+              elevation={0}
+              sx={{
+                borderRadius: "1rem",
+                p: "1rem",
+
+                width: "100%",
+
+                boxSizing: "border-box",
+                border: "1px dashed #ddd",mb:3
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  pb: 1,
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: 1,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      borderRadius: "50%",
+                      bgcolor: "#eef4ff",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id="Science-Molecule--Streamline-Ultimate" height="24" width="24">
+  <desc>
+    Science Molecule Streamline Icon: https://streamlinehq.com
+  </desc>
+  <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M9.759 12.009c0 0.5968 0.23706 1.1691 0.659 1.591 0.422 0.422 0.9943 0.659 1.591 0.659s1.169 -0.237 1.591 -0.659c0.4219 -0.4219 0.659 -0.9942 0.659 -1.591 0 -0.5967 -0.2371 -1.169 -0.659 -1.591 -0.422 -0.42191 -0.9943 -0.65897 -1.591 -0.65897s-1.169 0.23706 -1.591 0.65897c-0.42194 0.422 -0.659 0.9943 -0.659 1.591Z" stroke-width="1.5"></path>
+  <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M1.40269 22.6163c0.98456 0.9846 3.04628 0.8113 5.7316 -0.4816 2.68532 -1.2929 5.77431 -3.5996 8.58731 -6.4127 2.813 -2.813 5.1198 -5.90197 6.4127 -8.58728 1.2929 -2.68532 1.4662 -4.74704 0.4816 -5.73161 -0.9846 -0.984561 -3.0463 -0.811324 -5.7316 0.48161C14.199 3.17765 11.11 5.48436 8.29698 8.29741 5.48393 11.1105 3.17722 14.1994 1.88429 16.8847 0.591359 19.57 0.418121 21.6318 1.40269 22.6163Z" stroke-width="1.5"></path>
+  <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M8.29698 15.7221c1.39289 1.3928 2.86812 2.6761 4.34152 3.7764 1.4735 1.1003 2.9162 1.9961 4.2458 2.6363 1.3297 0.6402 2.5202 1.0122 3.5036 1.0949 0.9834 0.0826 1.7405 -0.1258 2.228 -0.6133 0.4875 -0.4875 0.6959 -1.2446 0.6133 -2.228 -0.0826 -0.9835 -0.4547 -2.174 -1.0949 -3.5036 -0.6402 -1.3297 -1.536 -2.7724 -2.6363 -4.2458 -1.1003 -1.4734 -2.3835 -2.94868 -3.7764 -4.34156 -1.3929 -1.39288 -2.8681 -2.67611 -4.3416 -3.7764 -1.47337 -1.1003 -2.91609 -1.99612 -4.24573 -2.63632C5.80463 1.24452 4.61411 0.872484 3.63068 0.789849 2.64724 0.707214 1.89016 0.9156 1.40265 1.40311 0.915142 1.89062 0.706756 2.6477 0.789391 3.63114c0.082635 0.98343 0.454669 2.17395 1.094869 3.50359 0.6402 1.32964 1.53602 2.77235 2.63632 4.24577 1.1003 1.4734 2.38352 2.9487 3.7764 4.3416Z" stroke-width="1.5"></path>
+</svg>
+                  </Box>
+
+                  <Typography
+                    variant="h6"
+                    sx={{ fontFamily: "system-ui", color: "#0b0317d8" }}
+                  >
+                    Skills
+                  </Typography>
+                </Box>
+
+                <Button
+                  // onClick={()=>{setExperienceOpen(true)}}
+                  sx={{
+                    textTransform: "none",
+                    fontWeight: 600,
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                    id="Add-1--Streamline-Core"
+                    height="24"
+                    width="24"
+                  >
+                    <desc>Add 1 Streamline Icon: https://streamlinehq.com</desc>
+                    <g id="add-1--expand-cross-buttons-button-more-remove-plus-add-+-mathematics-math">
+                      <path
+                        id="Vector"
+                        stroke="#000000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M7 0.5v13"
+                        stroke-width="0.8"
+                      ></path>
+                      <path
+                        id="Vector_2"
+                        stroke="#000000f8"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M0.5 6.95996h13"
+                        stroke-width="0.8"
+                      ></path>
+                    </g>
+                  </svg>
+                </Button>
+              </Box>
+
+              <Divider />
+
+              <Box
+                sx={{
+                  pt: "1rem",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  textAlign: "center",
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  sx={{ color: "#000000af", fontFamily: "monospace", mb: 0.6 }}
+                >
+                  No skills added yet
+                </Typography>
+
+                <Typography
+                  color="text.secondary"
+                  sx={{
+                    maxWidth: 500,
+                    fontFamily: "monospace",
+
+                    color: "#00000077",
+                    fontSize: "0.8rem",
+                  }}
+                >
+                  Add your skills to showcase your strengths and expertise.
+                </Typography>
+
+                <Button
+                  startIcon={<AddIcon />}
+                  // onClick={()=>{setExperienceOpen(true)}}
+
+                  sx={{
+                    textTransform: "none",
+                    fontWeight: 400,
+                    fontFamily: "system-ui",
+                    border: "1px solid #ddd",
+                    mt: 2,
+                    borderRadius: "15px",
+                  }}
+                >
+                  Add Skills
+                </Button>
+              </Box>
+            </Paper>
+
+
+
           </Box>
         </Box>
 
