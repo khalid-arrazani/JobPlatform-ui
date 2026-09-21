802e3964374cf83ef4a1cff9d39a47b1ec0acbf6
khalid <karazani2003@gmail.com>
2026-09-21 20:10:42 +0100

    1111

--- src/components/profile/JobSeekerProfile/Mobile/JobSeekerProfileMobile.jsx
+++ src/components/profile/JobSeekerProfile/Mobile/JobSeekerProfileMobile.jsx
@@ -231,6 +231,7 @@ export default function JobSeekerProfileMobile() {
                   <Box  sx={{width:"100%",height:"5rem",borderBottom:"solid #ddd", display:"flex",alignItems:"center"}}>
                    <Typography  sx={{fontFamily:'monospace',fontSize:"1.3rem"}} >Edit Profile Info</Typography>
                   </Box>
+
                 </Box>
               </Modal>
             </Box>
