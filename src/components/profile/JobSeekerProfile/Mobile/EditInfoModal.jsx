1ed33352b6eeb21e3f919fcc7371537db715a517
khalid <karazani2003@gmail.com>
2026-09-22 12:02:51 +0100

    111

--- src/components/profile/JobSeekerProfile/Mobile/EditInfoModal.jsx
+++ src/components/profile/JobSeekerProfile/Mobile/EditInfoModal.jsx
@@ -59,30 +59,10 @@ export default function EditInfoModal({ setOpen, open }) {
           </Box>
 
           <Box sx={{ flex: 1 }}>
-            <Box sx={{ mb: "1rem" }}>
-              <Typography
-                sx={{
-                  fontSize: "1rem",
-                  fontWeight: 600,
-                  mb: "0.5rem",
-                  color: "#070e1ee5",
-                  fontFamily: "monospace",
-                }}
-              >
-                Full name
-              </Typography>
 
-              <TextField
-                value={about}
-                onChange={(e) => setAbout(e.target.value)}
-                fullWidth
-                slotProps={{
-                  htmlInput: {
-                    maxLength: 700,
-                  },
-                }}
-              />
-            </Box>
+
+
+
 
             <Box sx={{ mb: "1rem" }}>
               <Typography
@@ -94,19 +74,75 @@ export default function EditInfoModal({ setOpen, open }) {
                   fontFamily: "monospace",
                 }}
               >
-                Headline
+                Location
               </Typography>
 
-              <TextField
-                value={about}
-                onChange={(e) => setAbout(e.target.value)}
-                fullWidth
-                slotProps={{
-                  htmlInput: {
-                    maxLength: 700,
-                  },
+              <Box
+                sx={{
+                  display: "flex",
+                  border: "solid #ddd 1px",
+                  alignItems: "center",
+                  borderRadius: "10px",
                 }}
-              />
+              >
+                <Box
+                  sx={{
+                    height: "3.4rem",
+                    width: "3.4rem",
+                    
+                    
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRight:"solid #ddd 1px"
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#000000"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    id="Location--Streamline-Tabler"
+                    height="24"
+                    width="24"
+                  >
+                    
+                    <path
+                      d="m21 3 -6.5 18a0.55 0.55 0 0 1 -1 0L10 14l-7 -3.5a0.55 0.55 0 0 1 0 -1L21 3"
+                      stroke-width="1.8"
+                    ></path>
+                  </svg>
+
+                </Box>
+
+                <TextField
+                  value={about}
+                  onChange={(e) => setAbout(e.target.value)}
+                  fullWidth
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: "0.3rem",
+                      fontFamily: "monospace",
+                      fontSize: "1.4rem",
+                    },
+                    "& .css-18p5xg2-MuiNotchedOutlined-root-MuiOutlinedInput-notchedOutline":
+                      {
+                        border: "none",
+                      },
+                    "& .MuiInputBase-input": {
+                      py: 0,
+                    },
+                    border: "none",
+                  }}
+                  slotProps={{
+                    htmlInput: {
+                      maxLength: 700,
+                    },
+                  }}
+                />
+              </Box>
             </Box>
 
             <Box sx={{ mb: "1rem" }}>
@@ -127,12 +163,39 @@ export default function EditInfoModal({ setOpen, open }) {
                   display: "flex",
                   border: "solid #ddd 1px",
                   alignItems: "center",
+                  borderRadius: "10px",
                 }}
               >
                 <Box
-                  sx={{ height: "3.4rem", width: "3.4rem", bgcolor: "#ddd" }}
+                  sx={{
+                    height: "3.4rem",
+                    width: "3.4rem",
+                    
+                    
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRight:"solid #ddd 1px"
+                  }}
                 >
-                  
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#000000"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    id="Location--Streamline-Tabler"
+                    height="24"
+                    width="24"
+                  >
+                    
+                    <path
+                      d="m21 3 -6.5 18a0.55 0.55 0 0 1 -1 0L10 14l-7 -3.5a0.55 0.55 0 0 1 0 -1L21 3"
+                      stroke-width="1.8"
+                    ></path>
+                  </svg>
+
                 </Box>
 
                 <TextField
@@ -143,22 +206,17 @@ export default function EditInfoModal({ setOpen, open }) {
                     "& .MuiOutlinedInput-root": {
                       borderRadius: "0.3rem",
                       fontFamily: "monospace",
-                      fontSize:"1.4rem",
+                      fontSize: "1.4rem",
                     },
                     "& .css-18p5xg2-MuiNotchedOutlined-root-MuiOutlinedInput-notchedOutline":
                       {
                         border: "none",
                       },
-                    "& .MuiInputBase-input":
-                      {
-                        py:0
-                      },
+                    "& .MuiInputBase-input": {
+                      py: 0,
+                    },
                     border: "none",
-                    
                   }}
-
-
-
                   slotProps={{
                     htmlInput: {
                       maxLength: 700,
@@ -167,6 +225,94 @@ export default function EditInfoModal({ setOpen, open }) {
                 />
               </Box>
             </Box>
+
+
+            <Box sx={{ mb: "1rem" }}>
+              <Typography
+                sx={{
+                  fontSize: "1rem",
+                  fontWeight: 600,
+                  mb: "0.5rem",
+                  color: "#070e1ee5",
+                  fontFamily: "monospace",
+                }}
+              >
+                Location
+              </Typography>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  border: "solid #ddd 1px",
+                  alignItems: "center",
+                  borderRadius: "10px",
+                }}
+              >
+                <Box
+                  sx={{
+                    height: "3.4rem",
+                    width: "3.4rem",
+                    
+                    
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRight:"solid #ddd 1px"
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#000000"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    id="Location--Streamline-Tabler"
+                    height="24"
+                    width="24"
+                  >
+                    
+                    <path
+                      d="m21 3 -6.5 18a0.55 0.55 0 0 1 -1 0L10 14l-7 -3.5a0.55 0.55 0 0 1 0 -1L21 3"
+                      stroke-width="1.8"
+                    ></path>
+                  </svg>
+
+                </Box>
+
+                <TextField
+                  value={about}
+                  onChange={(e) => setAbout(e.target.value)}
+                  fullWidth
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: "0.3rem",
+                      fontFamily: "monospace",
+                      fontSize: "1.4rem",
+                    },
+                    "& .css-18p5xg2-MuiNotchedOutlined-root-MuiOutlinedInput-notchedOutline":
+                      {
+                        border: "none",
+                      },
+                    "& .MuiInputBase-input": {
+                      py: 0,
+                    },
+                    border: "none",
+                  }}
+                  slotProps={{
+                    htmlInput: {
+                      maxLength: 700,
+                    },
+                  }}
+                />
+              </Box>
+            </Box>
+
+
+
+
+
+
           </Box>
         </Box>
       </Modal>
